2c24aa0339a85a266cd6756dd4e19cf7ecacddde
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-11-18 16:55:21 +0900

    [Feat ✏️] isChecked를 별도로 분리후 재사용

--- src/Components/EditPage/Editor/Features/EditorCategory.tsx
+++ src/Components/EditPage/Editor/Features/EditorCategory.tsx
@@ -17,22 +17,26 @@ const EditorCategory: React.FC<EditorCategoryProps> = ({ categoryList, onSelecti
       <h6 className="category__title">카테고리 :</h6>
       <ul className="category__itembox">
         {categoryList &&
-          categoryList.map((item, idx) => (
-            <li key={idx} className="category__item">
-              <input
-                className="category__item-radio"
-                id={`categoryList-${idx}`}
-                value={item}
-                name="category"
-                type="radio"
-                onChange={selectedHandler}
-                checked={selected === item}
-              />
-              <label className="category__label" htmlFor={`categoryList-${idx}`}>
-                {item}
-              </label>
-            </li>
-          ))}
+          categoryList.map((item, idx) => {
+            const isChecked: boolean = selected === item;
+
+            return (
+              <li key={idx} className={`category__item${isChecked ? "-checked" : ""}`}>
+                <input
+                  className="category__item-radio"
+                  id={`categoryList-${idx}`}
+                  value={item}
+                  name="category"
+                  type="radio"
+                  onChange={selectedHandler}
+                  checked={isChecked}
+                />
+                <label className="category__label" htmlFor={`categoryList-${idx}`}>
+                  {item}
+                </label>
+              </li>
+            );
+          })}
       </ul>
     </form>
   );
