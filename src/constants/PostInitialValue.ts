0efe1d6e29ae6dd3935352c0d9bb86aa1094ea24
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-11-18 18:23:50 +0900

    [Refactoring ⚙️] 상수 별도 보관

---
+++ src/constants/PostInitialValue.ts
@@ -0,0 +1,13 @@
+import { PostDataType } from "../Types/Components/Edit/EditorTypes";
+
+export const newPost: PostDataType = {
+  id: "newPost",
+  createAt: "newPost",
+  updateAt: "newPost",
+  title: "제목 없음",
+  subtitle: "부제목 없음",
+  main: "## 본문 없음",
+  isPublic: true,
+  category: "",
+  tag: [],
+};
