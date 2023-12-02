881b6c949b86c529e4f8b617c7abaf316f462d0c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-02 17:14:33 +0900

    [Refactoring ⚙️] 네이밍 변경

--- src/Types/Components/Nav/NavMenubarTypes.ts
+++ src/Types/Components/Nav/NavMenubarTypes.ts
@@ -1,7 +1,7 @@
 import { IconType } from "react-icons";
 import { PostDataType } from "../Edit/EditorTypes";
 
-export interface MenubarItemProps {
+export interface NavMenubarItemProps {
   icon: IconType;
   title: string;
   category: string;
