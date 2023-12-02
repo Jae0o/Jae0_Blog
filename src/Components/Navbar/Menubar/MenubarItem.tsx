523e1e523eb0220f547dc6ef915d80e4c35ea33d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-02 14:18:14 +0900

    [Feat ✏️] 라우터 연결

--- src/Components/Navbar/Menubar/MenubarItem.tsx
+++ src/Components/Navbar/Menubar/MenubarItem.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "../../../CSS/Navbar/Menubar/MenubarItem.css";
 import { MenubarItemProps } from "../../../Types/Components/Nav/MenubarTypes";
+import { NavigateFunction, useNavigate } from "react-router-dom";
 
 const MenubarItem: React.FC<MenubarItemProps> = ({ icon: ICON, title, category, list }) => {
+  const navigate: NavigateFunction = useNavigate();
+
+  const onNavigate = () => {
+    navigate(`/category/${category}`, { state: { list } });
+  };
+
   return (
-    <li className="menubar__item" value={category}>
+    <li className="menubar__item" value={category} onClick={onNavigate}>
       <div className="menubar__itemIcon-box">
         <ICON className="menubat__itemIcon-icon" />
         <p className="menubar__listCount">{list.length}</p>
