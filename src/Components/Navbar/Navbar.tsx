103fffcf03ba37bcec727cb7b041e87fb68cdb3f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-02 14:13:41 +0900

    [Refactoring ⚙️] 상수 보관 및 네이밍 전체 변경

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../../CSS/Navbar/Navbar.css";
-import NavProfileCard from "./ProfileCard/NavProfileCard";
+import NavProfile from "./ProfileCard/NavProfile";
 import Menubar from "./Menubar/Menubar";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="nav">
-      <NavProfileCard />
+      <NavProfile />
       <Menubar />
     </nav>
   );
---
+++ src/constants/URL.ts
@@ -0,0 +1,8 @@
+interface ProfileConstansType {
+  THUMBNAIL: string;
+}
+
+export const PROFILE: ProfileConstansType = {
+  THUMBNAIL:
+    "https://res.cloudinary.com/dalxgxu2o/image/upload/c_scale,h_120,q_100,w_120/v1699980818/IMG_0508_mke9kp.gif",
+};
