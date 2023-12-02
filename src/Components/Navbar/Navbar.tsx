a37daaefe8baa34d854418e99cfbcf4fd719c3df
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-02 14:15:08 +0900

    [Rename 🏷️] 폴더명 변경

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../CSS/Navbar/Navbar.css";
-import NavProfile from "./ProfileCard/NavProfile";
+import NavProfile from "./Profile/NavProfile";
 import Menubar from "./Menubar/Menubar";
 
 const Navbar: React.FC = () => {
