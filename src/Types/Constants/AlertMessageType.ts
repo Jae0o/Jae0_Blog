b13dbb7a3afd26b9e3b03efa7f0b24e60363de03
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-03 00:15:59 +0900

    [Refactoring ⚙️] Alert 상수의 타입 별도 관리

---
+++ src/Types/Constants/AlertMessageType.ts
@@ -0,0 +1,16 @@
+export interface AlertContextType {
+  TAG: string;
+  CATEGORY: string;
+}
+
+export interface AlertEditType {
+  UPLOAD_SUCCESS: string;
+  UPLOAD_FAIL: string;
+}
+
+export interface ErrorMessageTyps {
+  SET_OPTION_LIST: string;
+  GET_OPTION_LIST: string;
+  SET_POST: string;
+  GET_POST: string;
+}
--- src/constants/AlertMessage.ts
+++ src/constants/AlertMessage.ts
@@ -1,11 +1,22 @@
-export const ALERT_CONTEXT_TAG: string = "Tag List 불러오기 과정중 문제가 발생했습니다. 🥹";
-export const ALERT_CONTEXT_CATEGORY: string =
-  "Category List 불러오기 과정중 문제가 발생했습니다. 🥹";
+import {
+  AlertContextType,
+  AlertEditType,
+  ErrorMessageTyps,
+} from "../Types/Constants/AlertMessageType";
 
-export const ALERT_EDIT_UPLOAD_SUCCESS: string = "업로드에 성공하였습니다! 👍";
-export const ALERT_EDIT_UPLOAD_FAIL: string = "업로드에 실패했습니다! 🖐️";
+export const ALERT_CONTEXT: AlertContextType = {
+  TAG: "Tag List 불러오기 과정중 문제가 발생했습니다. 🥹",
+  CATEGORY: "Category List 불러오기 과정중 문제가 발생했습니다. 🥹",
+};
 
-export const ERROR_MESSAGE_SET_OPTION_LIST: string = "을 추가하는 과정에서 ERROR 발생.";
-export const ERROR_MESSAGE_GET_OPTION_LIST: string = "을 불러오는 과정에서 ERROR 발생.";
-export const ERROR_MESSAGE_SET_POST: string = "Post를 업로드하는 중 ERROR 발생.";
-export const ERROR_MESSAGE_GET_POST: string = "Post를 불러오는 중 ERROE 발생.";
+export const ALERT_EDIT: AlertEditType = {
+  UPLOAD_SUCCESS: "업로드에 성공하였습니다! 👍",
+  UPLOAD_FAIL: "업로드에 실패했습니다! 🖐️",
+};
+
+export const ERROR_MESSAGE: ErrorMessageTyps = {
+  SET_OPTION_LIST: "을 추가하는 과정에서 ERROR 발생.",
+  GET_OPTION_LIST: "을 불러오는 과정에서 ERROR 발생.",
+  SET_POST: "Post를 업로드하는 중 ERROR 발생.",
+  GET_POST: "Post를 불러오는 중 ERROE 발생.",
+};
