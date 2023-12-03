76ed92ae16b03acc86c245775acd4c87e29fa9d2
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-03 15:53:07 +0900

    [Feat ✏️] random thumbnail 구현

--- src/constants/URL.ts
+++ src/constants/URL.ts
@@ -6,3 +6,16 @@ export const PROFILE: ProfileConstansType = {
   THUMBNAIL:
     "https://res.cloudinary.com/dalxgxu2o/image/upload/c_scale,h_120,q_100,w_120/v1699980818/IMG_0508_mke9kp.gif",
 };
+
+export const POST_BANNER_THUMBNAILS: string[] = [
+  "https://res.cloudinary.com/dalxgxu2o/image/upload/v1701585785/Blog%20Assets/Post%20Banner%20Thumbnail/banner-thumbnail-1_ms41wb.jpg",
+  "https://res.cloudinary.com/dalxgxu2o/image/upload/v1701585784/Blog%20Assets/Post%20Banner%20Thumbnail/banner-thumbnail-2_udfi4d.jpg",
+  "https://res.cloudinary.com/dalxgxu2o/image/upload/v1701585786/Blog%20Assets/Post%20Banner%20Thumbnail/banner-thumbnail-3_vbfhe4.jpg",
+  "https://res.cloudinary.com/dalxgxu2o/image/upload/v1701585782/Blog%20Assets/Post%20Banner%20Thumbnail/banner-thumbnail-4_bk6rs7.jpg",
+  "https://res.cloudinary.com/dalxgxu2o/image/upload/v1701585784/Blog%20Assets/Post%20Banner%20Thumbnail/banner-thumbnail-5_hqjmgy.jpg",
+  "https://res.cloudinary.com/dalxgxu2o/image/upload/v1701585782/Blog%20Assets/Post%20Banner%20Thumbnail/banner-thumbnail-6_npr3hw.jpg",
+  "https://res.cloudinary.com/dalxgxu2o/image/upload/v1701585783/Blog%20Assets/Post%20Banner%20Thumbnail/banner-thumbnail-7_dm12k8.png",
+  "https://res.cloudinary.com/dalxgxu2o/image/upload/v1701585783/Blog%20Assets/Post%20Banner%20Thumbnail/banner-thumbnail-8_oo0cp8.jpg",
+  "https://res.cloudinary.com/dalxgxu2o/image/upload/v1701585782/Blog%20Assets/Post%20Banner%20Thumbnail/banner-thumbnail-9_mzplgq.jpg",
+  "https://res.cloudinary.com/dalxgxu2o/image/upload/v1701585783/Blog%20Assets/Post%20Banner%20Thumbnail/banner-thumbnail-10_yfnw9l.jpg",
+];
