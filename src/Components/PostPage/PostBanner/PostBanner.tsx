0d52fc05f808212f40e5bccce93296bc77f588c6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-03 16:36:44 +0900

    [Fix 🪛] Text 가시성 확대

--- src/Components/PostPage/PostBanner/PostBanner.tsx
+++ src/Components/PostPage/PostBanner/PostBanner.tsx
@@ -3,13 +3,11 @@ import "../../../CSS/PostPage/PostBanner/PostBanner.css";
 import PostBannerDecotation from "./PostBannerDecoration";
 import { AdviceType } from "../../../Types/Constants/VariablesType";
 import { ADVICE_DEFAULT, ADVICE_LIST } from "../../../constants/variables";
-import { useParams } from "react-router-dom";
 import { POST_BANNER_THUMBNAILS } from "../../../constants/URL";
 
 const PostBanner: React.FC = () => {
   const [advice, setAdvice] = useState<AdviceType>(ADVICE_DEFAULT);
   const [thumbnail, setThumbnail] = useState<string>("");
-  const { category = "" } = useParams<string>();
 
   useEffect(function makeRandomBanner() {
     const randomNum: number = Math.random() * 10;
@@ -24,12 +22,9 @@ const PostBanner: React.FC = () => {
       <div
         className="ptbanner__container"
         style={{ backgroundImage: `url(${thumbnail})` }}>
-        <div className="ptbanner__info">
-          {category && <h1 className="ptbanner__title">{category}</h1>}
-          <div className="ptbanner__advice-container">
-            <h4 className="ptbanner__advice">{advice.advice}</h4>
-            <h6 className="ptbanner__author">{`- ${advice.author}`}</h6>
-          </div>
+        <div className="ptbanner__advice-container">
+          <h4 className="ptbanner__advice">{advice.advice}</h4>
+          <h6 className="ptbanner__author">{`- ${advice.author}`}</h6>
         </div>
       </div>
     </div>
