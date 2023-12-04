b549e82bb1a5dc4aba139d4c8fcee7e419aaad89
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-04 18:25:39 +0900

    [Feat ✏️] PostList 연결

--- src/Components/PostPage/PostList/PostListItem.tsx
+++ src/Components/PostPage/PostList/PostListItem.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "../../../CSS/PostPage/PostList/PostListItem.css";
+import { PostListItemProps } from "../../../Types/Components/PostPage/PostListTypes";
 
-const PostListItem: React.FC = () => {
-  return <></>;
+const PostListItem: React.FC<PostListItemProps> = ({ post }) => {
+  return (
+    <li>
+      <div> {post.title}</div>
+    </li>
+  );
 };
 
 export default PostListItem;
--- src/Types/Components/PostPage/PostPageType.ts
+++ src/Types/Components/PostPage/PostPageType.ts
@@ -1,12 +1,4 @@
-import { PostDataType } from "../Edit/EditorTypes";
-
 export interface PostBannerParamsType {
   id?: string;
   category?: string;
-}
-
-export interface PostListLocationType {
-  state: {
-    list: PostDataType[];
-  };
 }
