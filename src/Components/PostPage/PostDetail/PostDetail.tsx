f698968f393dc261661b3fd8f76bbcc21bfe7b4a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-10 20:10:26 +0900

    [Refactroing ⚙️] Editor 페이지 경로값 변경

--- src/Components/PostPage/PostDetail/PostDetail.tsx
+++ src/Components/PostPage/PostDetail/PostDetail.tsx
@@ -1,27 +1,36 @@
 import React, { useEffect, useState } from "react";
 import "../../../CSS/PostPage/PostDetail/PostDetail.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { getPost } from "../../../API/FirebaseDB";
 import { PostData } from "../../../Types/API/FirebaseTypes";
 import { FetchPostFunc } from "../../../Types/Components/PostPage/PostPageType";
+import { OnClickEvent } from "../../../Types/EventTypes";
 
 const PostDetail: React.FC = () => {
   const [post, setPost] = useState<PostData>();
   const { category = "", id = "" } = useParams();
 
-  useEffect(() => {
-    const fetchPost: FetchPostFunc = async (category, pathId) => {
-      const resPost: PostData = await getPost(category, pathId);
-      setPost(resPost);
-    };
+  useEffect(
+    function inintialPost() {
+      const fetchPost: FetchPostFunc = async (category, pathId) => {
+        const resPost: PostData = await getPost(category, pathId);
+        setPost(resPost);
+      };
 
-    fetchPost(category, id);
-  }, [category, id]);
+      fetchPost(category, id);
+    },
+    [category, id],
+  );
+
+  const navigate = useNavigate();
+  const toEditPage: OnClickEvent = () => {
+    navigate(`/editor/${category}/${id}`);
+  };
 
   console.log(post);
   return (
     <>
-      <button>수정 하기</button>
+      <button onClick={toEditPage}>수정 하기</button>
     </>
   );
 };
