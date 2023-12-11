cb671e2f84e579dd0ea2028d471a04856503eb26
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-12 01:06:55 +0900

    [Fix 🪛] 데이터 업로드 관련 수정

--- src/Pages/Edit.tsx
+++ src/Pages/Edit.tsx
@@ -3,18 +3,14 @@ import "../CSS/EditPage/Edit.css";
 import EditPage from "../Components/EditPage/Editor/Editor";
 import EditSubPage from "../Components/EditPage/Sub/EditSubPage";
 import { UploadPost } from "../Types/Components/Edit/EditorTypes";
-// import { PostData } from "../Types/API/FirebaseTypes";
 import { setPost } from "../API/FirebaseDB";
 import { OnUpdateOption } from "../Types/Components/Edit/EditSubTypes";
 import { useNavigate, useParams } from "react-router-dom";
-// import { NEW_POST } from "../constants/variables";
 import UploadPage from "../Components/UploadPage";
 import { ALERT_EDIT } from "../constants/AlertMessage";
 import { ContextCategoryList } from "../Context/ContextCategory";
 import { ContextTagList } from "../Context/ContextTagList";
-
 import { ContextIsLoading } from "../Context/ContextIsLoading";
-// import { FetchPostFunc } from "../Types/Components/PostPage/PostPageType";
 
 const Edit: React.FC = () => {
   const { categoryList, updateCategoryList } = useContext(ContextCategoryList);
@@ -45,7 +41,7 @@ const Edit: React.FC = () => {
         navigate("/");
       })
       .catch(() => {
-        alert(ALERT_EDIT.UPLOAD_SUCCESS);
+        alert(ALERT_EDIT.UPLOAD_FAIL);
       })
       .finally(() => updateIsLoading(false));
   };
