11ec3d3e5b29cc0d5b17c433eb8658a126119093
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-11 18:31:17 +0900

    [Refactoring ⚙️] 렌더링 2번 발생 수정

--- src/Components/PostPage/PostBanner/PostBanner.tsx
+++ src/Components/PostPage/PostBanner/PostBanner.tsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from "react";
 import "../../../CSS/PostPage/PostBanner/PostBanner.css";
 import PostBannerDecotation from "./PostBannerDecoration";
-import { Advice } from "../../../Types/Constants/VariablesType";
 import { ADVICE_DEFAULT, ADVICE_LIST } from "../../../constants/variables";
 import { POST_BANNER_THUMBNAILS } from "../../../constants/URL";
+import { BannerInfo } from "../../../Types/Components/PostPage/PostPageType";
 
 const PostBanner: React.FC = () => {
-  const [advice, setAdvice] = useState<Advice>(ADVICE_DEFAULT);
-  const [thumbnail, setThumbnail] = useState<string>("");
+  const [bannerInfo, setBannerInfo] = useState<BannerInfo>({
+    advice: ADVICE_DEFAULT,
+    thumbnail: "",
+  });
 
   useEffect(function makeRandomBanner() {
     const randomNum: number = Math.random() * 10;
     const randomIndex: number = Math.floor(randomNum);
-    setAdvice(ADVICE_LIST[randomIndex]);
-    setThumbnail(POST_BANNER_THUMBNAILS[randomIndex]);
+    setBannerInfo({
+      advice: ADVICE_LIST[randomIndex],
+      thumbnail: POST_BANNER_THUMBNAILS[randomIndex],
+    });
   }, []);
 
   return (
@@ -21,10 +25,10 @@ const PostBanner: React.FC = () => {
       <PostBannerDecotation />
       <div
         className="ptbanner__container"
-        style={{ backgroundImage: `url(${thumbnail})` }}>
+        style={{ backgroundImage: `url(${bannerInfo.thumbnail})` }}>
         <div className="ptbanner__advice-container">
-          <h4 className="ptbanner__advice">{advice.advice}</h4>
-          <h6 className="ptbanner__author">{`- ${advice.author}`}</h6>
+          <h4 className="ptbanner__advice">{bannerInfo.advice.advice}</h4>
+          <h6 className="ptbanner__author">{`- ${bannerInfo.advice.author}`}</h6>
         </div>
       </div>
     </div>
--- src/Types/Components/PostPage/PostPageType.ts
+++ src/Types/Components/PostPage/PostPageType.ts
@@ -1,6 +1,13 @@
+import { Advice } from "../../Constants/VariablesType";
+
 export interface PostBanner {
   id?: string;
   category?: string;
+}
+
+export interface BannerInfo {
+  advice: Advice;
+  thumbnail: string;
 }
 
 export type FetchPostFunc = (category: string, pathId: string) => void;
