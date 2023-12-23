3b84dc970f00db1650c4c1336160d3c44c543cbe
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2023-12-23 22:59:01 +0900

    [Refactoring ⚙️] PostList & PostBanner 구조 변경

--- src/Components/PostPage/PostList/PostList.tsx
+++ src/Components/PostPage/PostList/PostList.tsx
@@ -1,13 +1,21 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./PostList.Style.css";
 import { useParams } from "react-router-dom";
 import PostListItem from "./Components/PostListItem";
 import { getAllPostsList, getPostsList } from "../../../API/FirebaseDB";
 import { FetchPostsList } from "./PostList.Types";
 import { PostData } from "../../../API/Firebase.Types";
+import { ADVICE_DEFAULT, ADVICE_LIST } from "../../../constants/variables";
+import { BannerInfo } from "../PostPageType";
+import { POST_BANNER_THUMBNAILS } from "../../../constants/URL";
+import PostBanner from "../PostBanner/PostBanner";
 
-const PostList: React.FC = () => {
+const PostList = (): JSX.Element => {
   const [postsList, setPostsList] = useState<PostData[]>([]);
+  const [bannerInfo, setBannerInfo] = useState<BannerInfo>({
+    advice: ADVICE_DEFAULT,
+    thumbnail: "",
+  });
 
   const { category = "ALL" } = useParams();
 
@@ -25,16 +33,39 @@ const PostList: React.FC = () => {
     fetchPostsList();
   }, [category]);
 
+  useEffect(
+    function makeRandomBanner() {
+      const randomNum: number = Math.random() * 10;
+      const randomIndex: number = Math.floor(randomNum);
+      setBannerInfo({
+        advice: ADVICE_LIST[randomIndex],
+        thumbnail: POST_BANNER_THUMBNAILS[randomIndex],
+      });
+    },
+    [category],
+  );
+
   return (
-    <ul className="outlet__ptlist">
-      {postsList &&
-        postsList.map(post => (
-          <PostListItem
-            post={post}
-            key={post.id}
-          />
-        ))}
-    </ul>
+    <>
+      <div className="ptlist__banner">
+        <PostBanner
+          thumbnail={bannerInfo.thumbnail}
+          mainText={bannerInfo.advice.advice}
+          subText={`- ${bannerInfo.advice.author}`}
+        />
+      </div>
+      <div className="ptlist__content">
+        <ul className="ptlist__list-container">
+          {postsList &&
+            postsList.map(post => (
+              <PostListItem
+                post={post}
+                key={post.id}
+              />
+            ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
