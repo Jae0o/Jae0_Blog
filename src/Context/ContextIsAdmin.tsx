4e664a94c6955e8c90adefa77514bf41057fc4d3
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-22 15:50:53 +0900

    [Feat ✏️] Login 관련 context 연결

--- src/Context/ContextIsAdmin.tsx
+++ src/Context/ContextIsAdmin.tsx
@@ -1,3 +1,23 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { ContextProps, IsAdminContext, Login } from "./Context.Types";
+import { login } from "../API/FirebaseAuth";
 
-export const ContextIsAdmin = createContext({});
+export const ContextIsAdmin = createContext<IsAdminContext>({
+  isAdminUser: false,
+  login: () => {},
+});
+
+export const ContextIsAdminUser = ({ children }: ContextProps) => {
+  const [isAdminUser, setIsAdminUser] = useState(false);
+
+  const Login = ({ email, password }: Login) => {
+    login(email, password);
+    setIsAdminUser(true);
+  };
+
+  return (
+    <ContextIsAdmin.Provider value={{ isAdminUser: isAdminUser, login: Login }}>
+      {children}
+    </ContextIsAdmin.Provider>
+  );
+};
