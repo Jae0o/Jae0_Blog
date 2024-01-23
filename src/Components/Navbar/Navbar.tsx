c38a25d41b6f3153defab905e773b0a502fa1634
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-23 18:15:41 +0900

    [Refactoring ⚙️] 기존 Navbar 스타일 전체 개선

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Navbar.Style.css";
 import NavProfile from "./components/Profile/NavProfile";
 import NavMenubar from "./components/Menubar/NavMenubar";
+import NavbarFooter from "./components/NavbarFooter/NavbarFooter";
 
 const Navbar = (): React.ReactNode => {
   return (
     <nav className="nav">
       <NavProfile />
       <NavMenubar />
+      <NavbarFooter />
     </nav>
   );
 };
--- src/Components/Navbar/components/NavbarFooter/NavbarFooter.tsx
+++ src/Components/Navbar/components/NavbarFooter/NavbarFooter.tsx
@@ -1,7 +1,13 @@
 import "./NavbarFooter.css";
 
 const NavbarFooter = () => {
-  return <footer className="nav__footer"></footer>;
+  return (
+    <footer className="navbar__footer">
+      <button></button>
+      <button></button>
+      <button></button>
+    </footer>
+  );
 };
 
 export default NavbarFooter;
