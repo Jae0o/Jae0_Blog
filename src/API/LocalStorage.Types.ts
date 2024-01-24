98dacd0cc0f14d5793a4cf11ec96b25594d84916
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-25 01:43:44 +0900

    [Remove 🗑️] 미사용 파일 제거

--- src/API/LocalStorage.Types.ts
+++
@@ -1,7 +0,0 @@
-import { PostData } from "./Firebase.Types";
-
-export type SetLocal = (post: PostData) => void;
-
-export type RemoveLocal = (id: string) => void;
-
-export type GetLocal = (id: string) => PostData | boolean;
