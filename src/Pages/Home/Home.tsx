9364149d0ae7913b338e74fcd76b06fd08e623c0
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-24 20:31:55 +0900

    [Feat ✏️] 이미지 리사이징 기능 추가

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Home.Style.css";
+import { ContextAuthUser } from "../../Context/ContextAuthUser";
 
 const Home = (): React.ReactNode => {
+  const { login } = useContext(ContextAuthUser);
+
+  login({
+    email: "leey153@naver.com",
+    password: "dlwodud2954",
+  });
+
   return <section className="outlet__home"></section>;
 };
 export default Home;
