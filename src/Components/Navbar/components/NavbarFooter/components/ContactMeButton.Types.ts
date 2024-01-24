e755d38b96a31ea388c7e9cc6d0d39569936294e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-24 16:13:54 +0900

    [Feat ✏️] Navbar footer Contact 제작

---
+++ src/Components/Navbar/components/NavbarFooter/components/ContactMeButton.Types.ts
@@ -0,0 +1,6 @@
+import React from "react";
+
+export interface ContactMeButtonProps {
+  url: string;
+  Icon: React.ReactNode;
+}
---
+++ src/Components/Navbar/components/NavbarFooter/components/ContactMeButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { ContactMeButtonProps } from "./ContactMeButton.Types";
+import "./ContactMeButton.styles.css";
+
+const ContactMeButton = ({
+  url,
+  Icon,
+}: ContactMeButtonProps): React.ReactNode => {
+  return (
+    <a
+      href={url}
+      target="_blank"
+      rel="noreferrer noopener"
+      className="navbar__footer-link">
+      {Icon}
+    </a>
+  );
+};
+
+export default ContactMeButton;
