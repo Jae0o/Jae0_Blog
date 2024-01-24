2b359c55a6b870698f37074acffc20f781134d18
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-24 17:24:33 +0900

    [Change 🚜 ] 기존 Editor를 quill 로 변경

--- src/Pages/Edit/components/Editor/Components/EditorMD/EditorMD.tsx
+++ src/Pages/Edit/components/Editor/Components/EditorMD/EditorMD.tsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./EditorMD.Style.css";
-import MDEditor from "@uiw/react-md-editor";
 import { EditorMDProps, OnChangeEditorMD } from "../../Editor.Types";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const EditorMD = ({ onTyping, state }: EditorMDProps): React.ReactNode => {
-  const onChangeMD: OnChangeEditorMD = value => {
-    onTyping("main", value);
+  const onChangeMD: OnChangeEditorMD = newValue => {
+    onTyping("main", newValue);
   };
 
+  const editorModules = useMemo(
+    () => ({
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, false] }],
+          ["bold", "italic", "underline", "strike"],
+          ["blockquote"],
+          [{ list: "ordered" }, { list: "bullet" }],
+          [{ color: [] }, { background: [] }],
+          [{ align: [] }, "link", "image"],
+        ],
+      },
+    }),
+    [],
+  );
+
   return (
-    <div className="editor__MD">
-      <div
-        className="MD"
-        data-color-mode="light">
-        <MDEditor
-          height={"100%"}
-          value={state}
-          onChange={newValue => onChangeMD(newValue as string)}
-        />
-      </div>
+    <div className="editor__layout">
+      <ReactQuill
+        className="editor__quill"
+        value={state}
+        onChange={onChangeMD}
+        modules={editorModules}
+      />
     </div>
   );
 };
