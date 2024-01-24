2d1242087b2f930afc408d2f442b75919eeb3e62
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-24 16:13:54 +0900

    [Feat ✏️] Navbar footer Contact 제작

--- src/Components/Navbar/components/NavbarFooter/NavbarFooter.tsx
+++ src/Components/Navbar/components/NavbarFooter/NavbarFooter.tsx
@@ -1,7 +1,26 @@
 import "./NavbarFooter.css";
+import ContactMeButton from "./components/ContactMeButton";
+import { FiGithub } from "react-icons/fi";
+import { FaInstagram } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
 const NavbarFooter = () => {
-  return <footer className="navbar__footer"></footer>;
+  return (
+    <footer className="navbar__footer">
+      <ContactMeButton
+        url="https://github.com/Jae0o"
+        Icon={<FiGithub />}
+      />
+      <ContactMeButton
+        url="https://www.instagram.com/jae_0o_/"
+        Icon={<FaInstagram />}
+      />
+      <ContactMeButton
+        url="mailto:leey153@naver.com"
+        Icon={<MdOutlineEmail />}
+      />
+    </footer>
+  );
 };
 
 export default NavbarFooter;
