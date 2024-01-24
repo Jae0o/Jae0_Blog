4d3eb9bd1cf19f006dfba3e7ece2191053b9c9bc
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-24 17:38:26 +0900

    [Feat ✏️] XSS 방어를 위한 기능 추가

--- src/Pages/Post/components/PostDetail/Components/PostDetailViewer/PostDetailViewer.tsx
+++ src/Pages/Post/components/PostDetail/Components/PostDetailViewer/PostDetailViewer.tsx
@@ -1,8 +1,11 @@
+import DOMPurify from "isomorphic-dompurify";
 import { PostDetailViewerProps } from "../../PostDetail.Types";
 import "./PostDetailViewer.Styles.css";
 
 const PostDetailViewer = ({ content }: PostDetailViewerProps) => {
-  return <div className="ptdetail__viewer-layout">{content}</div>;
+  const checkXSS = DOMPurify.sanitize(content);
+
+  return <div className="ptdetail__viewer-layout">{checkXSS}</div>;
 };
 
 export default PostDetailViewer;
