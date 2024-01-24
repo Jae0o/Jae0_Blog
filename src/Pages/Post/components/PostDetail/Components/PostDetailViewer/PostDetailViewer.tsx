2d7bf861426176cbae1e0e9d4e0d3dc6f6da51ac
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-24 18:09:10 +0900

    [Feat ✏️] Viewer 동작 구현

--- src/Pages/Post/components/PostDetail/Components/PostDetailViewer/PostDetailViewer.tsx
+++ src/Pages/Post/components/PostDetail/Components/PostDetailViewer/PostDetailViewer.tsx
@@ -1,11 +1,17 @@
 import DOMPurify from "isomorphic-dompurify";
 import { PostDetailViewerProps } from "../../PostDetail.Types";
 import "./PostDetailViewer.Styles.css";
+import "react-quill/dist/quill.core.css";
 
 const PostDetailViewer = ({ content }: PostDetailViewerProps) => {
   const checkXSS = DOMPurify.sanitize(content);
 
-  return <div className="ptdetail__viewer-layout">{checkXSS}</div>;
+  return (
+    <div
+      className="ptdetail__viewer-layout view ql-editor"
+      dangerouslySetInnerHTML={{ __html: checkXSS }}
+    />
+  );
 };
 
 export default PostDetailViewer;
