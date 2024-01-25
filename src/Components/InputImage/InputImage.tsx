9fbcc11bbe21e1c8c77c5d1dd02ba89a4fef43ed
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-25 17:38:32 +0900

    [Feat ✏️] image input에 대한 공용 컴포넌트 제작

---
+++ src/Components/InputImage/InputImage.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./InputImage.Styles.css";
+import { setImageStorage } from "../../API/FirebaseStore";
+
+interface InputImageProps {
+  inputRef: React.RefObject<HTMLInputElement>;
+  storagePath: string;
+  onSuccess: (url: string) => void;
+}
+
+const InputImage = ({ inputRef, storagePath, onSuccess }: InputImageProps) => {
+  const changeImage = async ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    if (!target.files) return;
+
+    const file = target.files[0];
+
+    const newImageUrl = await setImageStorage({
+      file,
+      path: storagePath,
+    });
+
+    if (!newImageUrl) {
+      // 실패에 대한 경고
+      return;
+    }
+
+    onSuccess(newImageUrl);
+  };
+
+  return (
+    <input
+      className="input__image-hide"
+      ref={inputRef}
+      type="file"
+      accept="image/*"
+      onChange={changeImage}
+    />
+  );
+};
+
+export default InputImage;
