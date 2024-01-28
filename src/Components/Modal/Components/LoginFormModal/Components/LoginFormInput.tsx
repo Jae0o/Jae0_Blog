608c1834e5bb275e902ce0f10655698926b1b88e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-28 19:00:22 +0900

    [Feat ✏️] Login Form 디자인 구현

--- src/Components/Modal/Components/LoginFormModal/Components/LoginFormInput.tsx
+++ src/Components/Modal/Components/LoginFormModal/Components/LoginFormInput.tsx
@@ -1,8 +1,21 @@
-export const LoginFormInput = () => {
+import "./LoginFormInput.Styles.css";
+
+interface LoginFormInputProps {
+  title: string;
+  type: "email" | "password";
+}
+
+export const LoginFormInput = ({ title, type }: LoginFormInputProps) => {
   return (
-    <div>
-      <input type="email" />
-      <p>이것은 경고 문제 테스트용이여라?</p>
+    <div className="loginForm__input">
+      <p className="loginForm__input-title">{title}</p>
+      <input
+        className="loginForm__input-element"
+        type={type}
+      />
+      <p className="loginForm__input-message">
+        이것은 경고 문제 테스트용이여라?
+      </p>
     </div>
   );
 };
--- src/Components/Modal/Components/LoginFormModal/LoginFormModal.tsx
+++ src/Components/Modal/Components/LoginFormModal/LoginFormModal.tsx
@@ -12,13 +12,24 @@ const LoginFormModal = ({ isShow, onClose }: LoginFormModalProps) => {
     <Modal
       isShow={isShow}
       width={32}
-      height={40}
+      height={33}
       onClose={onClose}
       clickAwayEnable={true}
       closeButtonEnable={true}>
       <h4 className="login__title">{"LOG IN !"}</h4>
-      <LoginFormInput />
-      <LoginFormInput />
+      <form className="login__form">
+        <LoginFormInput
+          title="EMAIL"
+          type="email"
+        />
+        <LoginFormInput
+          title="PASSWORD"
+          type="password"
+        />
+        <div className="login__action-container">
+          <button className="login__action-button">{"로그인"}</button>
+        </div>
+      </form>
     </Modal>
   );
 };
