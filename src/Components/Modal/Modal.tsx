d7fb1ecb973496ebd42990ae11f81869d9cf4188
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-28 16:49:35 +0900

    [Feat ✏️] 모달 기본 배경 구현

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,15 +1,32 @@
+import React from "react";
 import "./Modal.Styles.css";
 
 interface ModalProps {
+  children: React.ReactNode;
   width: number;
+  height: number;
 }
 
-const Modal = ({ width }: ModalProps) => {
+const Modal = ({ children, width, height }: ModalProps) => {
   console.log(width);
 
   return (
     <section className="modal__background">
-      <article className="modal__layout-outline"></article>
+      <article
+        className="modal__layout-outline"
+        style={{
+          width: `${width + 4}rem`,
+          height: `${height + 4}rem`,
+        }}>
+        <div
+          className="modal__layout"
+          style={{
+            width: `${width}rem`,
+            height: `${height}rem`,
+          }}>
+          {children}
+        </div>
+      </article>
     </section>
   );
 };
