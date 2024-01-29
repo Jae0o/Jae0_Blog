b27d64729c9821ca76ad0139728edcfa8392ed0d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-29 13:50:22 +0900

    [Feat ✏️] Alert Modal 구현

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "./Home.Style.css";
-import useModal from "../../Components/Modal/Hooks/useModal";
-import LoginFormModal from "../../Components/Modal/Components/LoginFormModal/LoginFormModal";
+import AlertModal from "../../Components/Modal/Components/AlertModal/AlertModal";
 
 const Home = (): React.ReactNode => {
-  const { isShowModal, openModal, closeModal } = useModal();
   return (
     <section className="outlet__home">
-      <button onClick={openModal}> open</button>
-      <LoginFormModal
-        isShow={isShowModal}
-        onClose={closeModal}
+      <AlertModal
+        isShow={true}
+        onClose={() => {}}
       />
     </section>
   );
