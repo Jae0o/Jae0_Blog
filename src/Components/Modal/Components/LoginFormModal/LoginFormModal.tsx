350ec660d12e827113f29096461a88bef794d60f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-29 19:54:52 +0900

    [Change 🚜 ] Login 함수 로직 변경

--- src/Components/Modal/Components/LoginFormModal/LoginFormModal.tsx
+++ src/Components/Modal/Components/LoginFormModal/LoginFormModal.tsx
@@ -3,15 +3,24 @@ import Modal from "../../Modal";
 import { LoginFormInput } from "./Components/LoginFormInput";
 import "./LoginFormModal.Styles.css";
 import { ContextAuthUser } from "../../../../Context/ContextAuthUser";
+import { firebaseLogin } from "../../../../API/FirebaseAuth";
+import AlertModal from "../AlertModal/AlertModal";
+import useModal from "../../Hooks/useModal";
 
 interface LoginFormModalProps {
   isShow: boolean;
   onClose: () => void;
+  onSuccess: () => void;
 }
 
-const LoginFormModal = ({ isShow, onClose }: LoginFormModalProps) => {
+const LoginFormModal = ({
+  isShow,
+  onClose,
+  onSuccess,
+}: LoginFormModalProps) => {
   const [loginInfo, setLoginInfo] = useState({ email: "", password: "" });
-  const { login } = useContext(ContextAuthUser);
+  const { setLoginUser } = useContext(ContextAuthUser);
+  const { isShowModal, openModal, closeModal } = useModal();
 
   const changeLoginInfo = ({
     type,
@@ -26,38 +35,53 @@ const LoginFormModal = ({ isShow, onClose }: LoginFormModalProps) => {
   const handleSubmitLogin = (e: React.FormEvent) => {
     e.preventDefault();
 
-    login(loginInfo);
+    firebaseLogin(loginInfo.email, loginInfo.password)
+      .then(({ user }) => {
+        setLoginUser(user);
+        onSuccess();
+      })
+      .catch(() => {
+        openModal();
+        console.log();
+      });
   };
 
   return (
-    <Modal
-      isShow={isShow}
-      width={32}
-      height={33}
-      onClose={onClose}
-      clickAwayEnable={true}
-      closeButtonEnable={true}>
-      <h4 className="login__title">{"LOG IN !"}</h4>
-      <form
-        className="login__form"
-        onSubmit={handleSubmitLogin}>
-        <LoginFormInput
-          title="EMAIL"
-          type="email"
-          value={loginInfo.email}
-          onChange={changeLoginInfo}
-        />
-        <LoginFormInput
-          title="PASSWORD"
-          type="password"
-          value={loginInfo.password}
-          onChange={changeLoginInfo}
-        />
-        <div className="login__action-container">
-          <button className="login__action-button">{"로그인"}</button>
-        </div>
-      </form>
-    </Modal>
+    <>
+      <Modal
+        isShow={isShow}
+        width={32}
+        height={33}
+        onClose={onClose}
+        clickAwayEnable={true}
+        closeButtonEnable={true}>
+        <h4 className="login__title">{"LOG IN !"}</h4>
+        <form
+          className="login__form"
+          onSubmit={handleSubmitLogin}>
+          <LoginFormInput
+            title="EMAIL"
+            type="email"
+            value={loginInfo.email}
+            onChange={changeLoginInfo}
+          />
+          <LoginFormInput
+            title="PASSWORD"
+            type="password"
+            value={loginInfo.password}
+            onChange={changeLoginInfo}
+          />
+          <div className="login__action-container">
+            <button className="login__action-button">{"로그인"}</button>
+          </div>
+        </form>
+      </Modal>
+
+      <AlertModal
+        isShow={isShowModal}
+        onClose={closeModal}
+      />
+    </>
   );
 };
 
--- src/Context/ContextAuthUser.tsx
+++ src/Context/ContextAuthUser.tsx
@@ -1,49 +1,31 @@
 import { createContext, useState } from "react";
 import { ContextProps } from "./Context.Types";
-import { firebaseLogin } from "../API/FirebaseAuth";
-import { auth } from "../API/Firebase";
-import { UserCredential } from "firebase/auth";
+import { User } from "firebase/auth";
 
+type SetLoginUser = (user: User) => void;
 interface AuthUserContext {
   isLoggedIn: boolean;
   authUserId: string;
-  login: (data: LoginInfo) => Promise<UserCredential>;
-}
-
-interface LoginInfo {
-  email: string;
-  password: string;
+  setLoginUser: SetLoginUser;
 }
 
 export const ContextAuthUser = createContext<AuthUserContext>({
   authUserId: "",
   isLoggedIn: false,
-  login: () => {},
+  setLoginUser: () => {},
 });
 
 export const ContextAuthUserProvider = ({ children }: ContextProps) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [authUserId, setAuthUserId] = useState<string>("");
 
-  const login = async ({ email, password }: LoginInfo) => {
-    if (isLoggedIn) {
-      return;
-    }
-
-    return firebaseLogin(email, password)
-      .then(res => {
-        setIsLoggedIn(true);
-        setAuthUserId(res.user.uid);
-      })
-      .catch(error => {
-        setIsLoggedIn(false);
-        throw Error(error);
-      });
+  const setLoginUser: SetLoginUser = async user => {
+    setAuthUserId(user.uid);
+    setIsLoggedIn(true);
   };
-  console.log(auth);
 
   return (
-    <ContextAuthUser.Provider value={{ isLoggedIn, login, authUserId }}>
+    <ContextAuthUser.Provider value={{ isLoggedIn, authUserId, setLoginUser }}>
       {children}
     </ContextAuthUser.Provider>
   );
