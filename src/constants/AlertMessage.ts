548f4c00692967d3377d4813fd31da1cb2c0cde7
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-30 12:17:45 +0900

    [Refactoring ⚙️] constants 타입 파일 제거

--- src/constants/AlertMessage.ts
+++ src/constants/AlertMessage.ts
@@ -1,16 +1,14 @@
-import { AlertContext, AlertEdit, ErrorMessage } from "./AlertMessage.Types";
-
-export const ALERT_CONTEXT: AlertContext = {
+export const ALERT_CONTEXT = {
   TAG: "Tag List 불러오기 과정중 문제가 발생했습니다. 🥹",
   CATEGORY: "Category List 불러오기 과정중 문제가 발생했습니다. 🥹",
 };
 
-export const ALERT_EDIT: AlertEdit = {
+export const ALERT_EDIT = {
   UPLOAD_SUCCESS: "업로드에 성공하였습니다! 👍",
   UPLOAD_FAIL: "업로드에 실패했습니다! 🖐️",
 };
 
-export const ERROR_MESSAGE: ErrorMessage = {
+export const ERROR_MESSAGE = {
   SET_OPTION_LIST: "을 추가하는 과정에서 ERROR 발생.",
   GET_OPTION_LIST: "을 불러오는 과정에서 ERROR 발생.",
   SET_POST: "Post를 업로드하는 중 ERROR 발생.",
