46cddcb15673e82269025400d7269d8c87167ae5
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-30 16:40:36 +0900

    [Feat ✏️] PostContext AlertModal 부착

--- src/Context/ContextPosts.tsx
+++ src/Context/ContextPosts.tsx
@@ -1,7 +1,9 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useCallback, useEffect, useState } from "react";
 import { getAllPostsList } from "../API/FirebaseDB";
 import { ContextProps } from "./Context.Types";
 import { PostData } from "../API/Firebase.Types";
+import useModal from "../Components/Modal/Hooks/useModal";
+import AlertModal from "../Components/Modal/Components/AlertModal/AlertModal";
 
 interface Posts {
   [key: string]: PostData[];
@@ -21,12 +23,13 @@ export const ContextPostsProvider = ({
   children,
 }: ContextProps): React.ReactNode => {
   const [posts, setPosts] = useState<Posts>({});
+  const { isShowModal, openModal, closeModal } = useModal();
 
-  const updatePosts = async () => {
+  const updatePosts = useCallback(async () => {
     const posts = await getAllPostsList();
 
     if (!posts) {
-      // 경고 모달
+      openModal();
       return;
     }
 
@@ -37,14 +40,24 @@ export const ContextPostsProvider = ({
       WORK: WORK_posts,
       DIARY: DIARY_posts,
     });
-  };
+  }, [openModal]);
 
   useEffect(() => {
     updatePosts();
-  }, []);
+  }, [updatePosts]);
+
+  const handleCloseAlertModal = () => {
+    closeModal();
+    updatePosts();
+  };
 
   return (
     <ContextPosts.Provider value={{ posts, updatePosts }}>
+      <AlertModal
+        isShow={isShowModal}
+        onClose={handleCloseAlertModal}
+        message="dsf"
+      />
       {children}
     </ContextPosts.Provider>
   );
