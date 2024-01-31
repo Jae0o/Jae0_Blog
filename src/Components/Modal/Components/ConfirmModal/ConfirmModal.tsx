ad1ab0e47cc796505c2c8119ada4582a1ff21e68
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-01 00:40:10 +0900

    [Feat ✏️] Confirm Modal 구현

--- src/Components/Modal/Components/ConfirmModal/ConfirmModal.tsx
+++ src/Components/Modal/Components/ConfirmModal/ConfirmModal.tsx
@@ -1,11 +1,21 @@
 import Modal from "../../Modal";
+import "./ConfirmModal.Styles.css";
 
 interface ConfirmModalProps {
   isShow: boolean;
+  message: string;
+  falseButtonText?: string;
+  trueButtonText?: string;
   onClose: (accept: boolean) => void;
 }
 
-const ConfirmModal = ({ isShow, onClose }: ConfirmModalProps) => {
+const ConfirmModal = ({
+  isShow,
+  message,
+  onClose,
+  trueButtonText = "확인",
+  falseButtonText = "취소",
+}: ConfirmModalProps) => {
   const handleCloseAcceptFalse = () => {
     onClose(false);
   };
@@ -17,11 +27,23 @@ const ConfirmModal = ({ isShow, onClose }: ConfirmModalProps) => {
     <Modal
       isShow={isShow}
       onClose={handleCloseAcceptFalse}
-      width={40}
-      height={40}
+      width={36}
+      height={20}
       clickAwayEnable={true}
       closeButtonEnable={true}>
-      <div></div>
+      <h4 className="confirm__message">{message}</h4>
+      <div className="confirm__actions">
+        <button
+          className="confirm__button"
+          onClick={handleCloseAcceptFalse}>
+          {falseButtonText}
+        </button>
+        <button
+          className="confirm__button"
+          onClick={handleCloseAcceptTrue}>
+          {trueButtonText}
+        </button>
+      </div>
     </Modal>
   );
 };
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,16 +1,8 @@
 import React from "react";
 import "./Home.Style.css";
-import ConfirmModal from "../../Components/Modal/Components/ConfirmModal/ConfirmModal";
 
 const Home = (): React.ReactNode => {
-  return (
-    <section className="outlet__home">
-      <ConfirmModal
-        isShow={true}
-        onClose={res => console.log(res)}
-      />
-    </section>
-  );
+  return <section className="outlet__home"></section>;
 };
 
 export default Home;
