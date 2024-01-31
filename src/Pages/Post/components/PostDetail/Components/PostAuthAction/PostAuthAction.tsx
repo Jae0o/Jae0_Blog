e9d9a7f8153125cb7f3f035c53418a78c9f5df7e
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-01-31 23:56:12 +0900

    [Design 🎨] PostAuthAction 스타일 구현

--- src/Pages/Post/components/PostDetail/Components/PostAuthAction/PostAuthAction.tsx
+++ src/Pages/Post/components/PostDetail/Components/PostAuthAction/PostAuthAction.tsx
@@ -3,7 +3,8 @@ import "./PostAuthAction.Styles.css";
 const PostAuthAction = () => {
   return (
     <aside className="ptdetail__auth-layout">
-      <button className="ptdetail__auth-button"></button>
+      <button className="ptdetail__auth-button">수정 하기</button>
+      <button className="ptdetail__auth-button">삭제 하기</button>
     </aside>
   );
 };
