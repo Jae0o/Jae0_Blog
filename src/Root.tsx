11999549cf67939a3c9f8860032f794e685b4c02
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-02 01:11:35 +0900

    [Fix 🪛] 반응형 CSS 파일 import 라인 수정

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,8 @@
 import "./Root.Style.css";
 import "./Styles/Quill.Styles.css";
+import "./Styles/Responsive400px.css";
+import "./Styles/Responsive700px.css";
+import "./Styles/Responsive1000px.css";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,3 @@
-import "./Styles/Responsive400px.css";
-import "./Styles/Responsive700px.css";
-import "./Styles/Responsive1000px.css";
 import "./index.css";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
