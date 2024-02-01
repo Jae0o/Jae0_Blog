8f4105046a099de68304a21536f943b5eedbd9d6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-02 00:31:48 +0900

    [Chore 🚀] import 규칙 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./Styles/Responsive400px.css";
 import "./Styles/Responsive700px.css";
 import "./Styles/Responsive1000px.css";
 import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
