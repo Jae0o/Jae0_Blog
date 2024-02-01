ed798584efc4ce8c82c718bed2cc5b705aaa8689
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-01 23:57:12 +0900

    [ Design ] 기존 반응형 적용 문제 수정 (#10)
    * [Design 🎨] PostListItem 반응형 작업
    
    * [Refactoring ⚙️] Post List 반응형 개선
    
    * [Refactoring ⚙️] PostBanner 텍스트란 반응형 개선
    
    * [Refactoring ⚙️] Post Detail Info 부분 반응형 수정
    
    * [Design 🎨] 1000px 에 대한 반응형 구현
    
    * [Design 🎨] 700px 이하에 대한 반응형
    
    * [Design 🎨] 400px 이하 반응형 개선
    
    * [Fix 🪛] 반응형 미적용 문제 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./Styles/Responsive400px.css";
+import "./Styles/Responsive700px.css";
+import "./Styles/Responsive1000px.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
