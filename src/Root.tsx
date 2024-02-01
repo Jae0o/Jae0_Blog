cd671e2bae14cf1209add220299f5a4243d49a08
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-01 22:44:56 +0900

    [Design 🎨] 1000px 에 대한 반응형 구현

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,12 +3,15 @@ import "./Root.Style.css";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import "./Styles/Quill.Styles.css";
+import "./Styles/Responsive1000px.css";
 
 const Root = (): React.ReactNode => {
   return (
     <main className="Root">
       <Navbar />
-      <Outlet />
+      <section className="outlet__layout">
+        <Outlet />
+      </section>
     </main>
   );
 };
