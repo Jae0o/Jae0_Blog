5d2dca0a50d5194e33f18a219c34577ae8241366
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-03 04:52:13 +0900

    [Bug 🐛] AwayClick 비활성화

--- src/Components/Navbar/Components/NavMenubar/NavMenubar.tsx
+++ src/Components/Navbar/Components/NavMenubar/NavMenubar.tsx
@@ -15,30 +15,30 @@ const NavMenubar = ({ isToggle, onToggle }: NavMenubarProps) => {
   const { posts } = useContext(ContextPosts);
   const menubarRef = useRef<HTMLUListElement>(null);
 
-  useEffect(() => {
-    const handleAwayClick = ({ currentTarget, target }: MouseEvent) => {
-      const { current } = menubarRef;
+  // useEffect(() => {
+  //   const handleAwayClick = ({ currentTarget, target }: MouseEvent) => {
+  //     const { current } = menubarRef;
 
-      if (!current || !isToggle || currentTarget === current) {
-        return;
-      }
+  //     if (!current || !isToggle || currentTarget === current) {
+  //       return;
+  //     }
 
-      if (
-        target instanceof HTMLElement &&
-        target.className === "navbar_dropdown_checkbox"
-      ) {
-        return;
-      }
+  //     if (
+  //       target instanceof HTMLElement &&
+  //       target.className === "navbar_dropdown_checkbox"
+  //     ) {
+  //       return;
+  //     }
 
-      onToggle();
-    };
+  //     onToggle();
+  //   };
 
-    document.addEventListener("mouseup", handleAwayClick);
+  //   document.addEventListener("mouseup", handleAwayClick);
 
-    return () => {
-      document.removeEventListener("mouseup", handleAwayClick);
-    };
-  }, [isToggle, onToggle]);
+  //   return () => {
+  //     document.removeEventListener("mouseup", handleAwayClick);
+  //   };
+  // }, [isToggle, onToggle]);
 
   return (
     <ul
