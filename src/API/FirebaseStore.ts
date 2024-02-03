968cc73af4ca663543485bacf5d5db7d360707ac
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-04 00:14:26 +0900

    [Refactoring ⚙️] 이미지 업로드 과정에서 리사이즈

--- src/API/FirebaseStore.ts
+++ src/API/FirebaseStore.ts
@@ -2,6 +2,7 @@ import { getDownloadURL, ref } from "firebase/storage";
 import { uploadBytes } from "firebase/storage";
 import { ERROR_MESSAGE } from "@/constants/AlertMessage";
 import { firebaseStorage } from "./Firebase";
+import resizeFile from "./Util/imageResizer";
 
 type SetImageStorage = (params: {
   file: File;
@@ -9,14 +10,23 @@ type SetImageStorage = (params: {
 }) => Promise<string | void>;
 
 export const setImageStorage: SetImageStorage = async ({ file, path }) => {
+  let resizedImage: File | Blob = file;
+
+  if (file.type !== "image/gif") {
+    resizedImage = await resizeFile(file);
+  }
+
   const storageRef = ref(firebaseStorage, `image/${path}`);
 
-  const newImage = await uploadBytes(storageRef, file)
+  const newImage = await uploadBytes(storageRef, resizedImage)
     .then(res => res)
     .catch(() => console.log(ERROR_MESSAGE.UPLOAD_STORAGE_IMAGE));
 
-  if (!newImage) return;
+  if (!newImage) {
+    return;
+  }
 
   const newImageUrl = await getDownloadURL(newImage.ref);
+
   return newImageUrl;
 };
