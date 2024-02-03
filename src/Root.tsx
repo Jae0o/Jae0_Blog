12a890530414553db999e371c03c33bec1594db9
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-02-04 01:39:26 +0900

    [Fix 🪛] 반응형 수정 테스트

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,9 +1,9 @@
 import "./Font/pretendard-subset.css";
 import "./Root.Style.css";
-import "./Styles/Quill.Styles.css";
-import "./Styles/Responsive400px.css";
-import "./Styles/Responsive700px.css";
-import "./Styles/Responsive1000px.css";
+// import "./Styles/Quill.Styles.css";
+// import "./Styles/Responsive400px.css";
+// import "./Styles/Responsive700px.css";
+// import "./Styles/Responsive1000px.css";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import AsideBar from "./Components/AsideBar/AsideBar";
