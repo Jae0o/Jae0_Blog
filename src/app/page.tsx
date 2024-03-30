900eca21f52524dc66dcf918e3807be3bf6cdbc3
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-03-30 22:17:21 +0900

    [Chore 🚀] Tailwind 기초 세팅

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main className="">ffds</main>;
+  return <main className=" bg-[--red]">ffds</main>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,29 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+
+    colors: {
+      black_500: "#323133",
+      black_300: "#434a54",
+
+      red_500: "#da4453",
+      red_300: " #ed5565",
+
+      yellow_500: "#ffce54",
+
+      green_500: "#a0d468",
+      green_700: "#8cc152",
+
+      white_500: "#fafafa",
+      white_700: "#e6e9ed",
+
+      grey_500: "#ccd1d9",
+      grey_700: "#656d78",
+
+      text_main: "#323133",
+      text_sub: "#ffce54",
+    },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
 export default config;
