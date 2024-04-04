81cd3cfe6d5c528290032f0dfd0d09be281c45c3
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-04 19:21:40 +0900

    [Design 🎨] daisyUi theme 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,38 @@
 import type { Config } from "tailwindcss";
 
+import theme from "daisyui/src/theming/themes";
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...theme.light,
+          primary: "#ffce54", // yellow
+          secondary: "#a0d468", // green
+          accent: "#da4453", // red
+          neutral: "#ff00ff",
+          "base-100": "#ffffff",
+        },
+
+        dark: {
+          ...theme.dark,
+          primary: "#ffce54", // yellow
+          secondary: "#a0d468", // green
+          accent: "#da4453", // red
+          neutral: "#ff00ff",
+          "base-100": "#ffffff",
+        },
       },
-    },
-
-    colors: {
-      black_500: "#323133",
-      black_300: "#434a54",
-
-      red_500: "#da4453",
-      red_300: " #ed5565",
-
-      yellow_500: "#ffce54",
-
-      green_500: "#a0d468",
-      green_700: "#8cc152",
-
-      white_500: "#fafafa",
-      white_700: "#e6e9ed",
-
-      grey_500: "#ccd1d9",
-      grey_700: "#656d78",
-
-      text_main: "#323133",
-      text_sub: "#ffce54",
-    },
+    ],
   },
+
   plugins: [
     require("tailwind-scrollbar-hide"),
     require("daisyui"),
