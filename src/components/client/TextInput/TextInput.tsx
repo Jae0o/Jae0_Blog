0a2f9e31e6a0559a2dd77c24b8510c70f69f0986
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-04-04 19:59:12 +0900

    [Feat ✏️] TextInput 구현

---
+++ src/components/client/TextInput/TextInput.tsx
@@ -0,0 +1,67 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+
+const pickInputSize = (size: "lg" | "md" | "sm" | "xs") => {
+  switch (size) {
+    case "md":
+      return "input-md";
+
+    case "lg":
+      return "input-lg";
+
+    case "sm":
+      return "input-sm";
+
+    case "xs":
+      return "input-xs";
+
+    default:
+      return "input-md";
+  }
+};
+
+const pickInputColor = (color: "green" | "yellow" | "red" | "naked") => {
+  switch (color) {
+    case "green":
+      return "input-secondary text-[--color-text_main]";
+
+    case "yellow":
+      return "input-primary text-[--color-text_main]";
+
+    case "red":
+      return "input-accent text-[--color-text_main]";
+
+    case "naked":
+      return "input-ghost text-[--color-text_main]";
+
+    default:
+      return "input-secondary text-[--color-text_main]";
+  }
+};
+
+interface TextInputProps {
+  register: UseFormRegisterReturn;
+  placeholder?: string;
+
+  className?: string;
+  size?: "lg" | "md" | "sm" | "xs";
+  color?: "green" | "yellow" | "red" | "naked";
+}
+
+const TextInput = ({
+  register,
+  size = "md",
+  color = "green",
+  placeholder = "",
+  className = "",
+}: TextInputProps) => {
+  return (
+    <input
+      type="text"
+      className={`input ${pickInputSize(size)} ${pickInputColor(color)} w-full ${className}`}
+      placeholder={placeholder}
+      {...register}
+    />
+  );
+};
+
+export default TextInput;
