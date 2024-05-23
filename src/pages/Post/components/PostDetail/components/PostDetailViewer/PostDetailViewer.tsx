9300147a09f5db79b4d55d3ff374bec6a2820df2
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-24 00:34:03 +0900

    [Fix 🪛] style import 누락 수정

--- src/pages/Post/components/PostDetail/components/PostDetailViewer/PostDetailViewer.tsx
+++ src/pages/Post/components/PostDetail/components/PostDetailViewer/PostDetailViewer.tsx
@@ -1,7 +1,7 @@
 import DOMPurify from "isomorphic-dompurify";
 
 import "./PostDetailViewer.style.css";
-import "@/Styles/Quill.style.css";
+import "@/styles/Quill.style.css";
 import "react-quill/dist/quill.snow.css";
 
 interface PostDetailViewerProps {
