85d7d9bbc58c74ef3241e2c95447511c4d5e78ec
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-23 18:45:50 +0900

    [Refactoring ⚙️] 라이브러리 lazy import 적용

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // 번들링을 시작할때 라이브러리들을 대상으로 코드 스플리팅을 실행함
+          if (id.includes("node_modules")) {
+            const module = id.split("node_modules/").pop().split("/")[0];
+
+            return `vendor/${module}`;
+          }
+        },
+      },
+    },
+  },
 });
