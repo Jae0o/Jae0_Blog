8061c02ba0d2adfe99cb2860a14c7affb395a9c1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-24 15:40:17 +0900

    [Feat ✏️] LoadingSpinner 구현

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -1,5 +1,23 @@
-const LoadingSpinner = () => {
-  return <div>LoadingSpinner</div>;
+import Lottie from "react-lottie-player";
+
+import LOADING_ANIMATION from "@/assets/loading_animation.json";
+
+interface LoadingSpinnerProps {
+  size?: string;
+}
+
+const LoadingSpinner = ({ size = "20rem" }: LoadingSpinnerProps) => {
+  return (
+    <Lottie
+      animationData={LOADING_ANIMATION}
+      style={{
+        width: size,
+        height: size,
+      }}
+      loop
+      play
+    />
+  );
 };
 
 export default LoadingSpinner;
