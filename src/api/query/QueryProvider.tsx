72a13654797fe2cdebacdba546bee907a58b4364
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-28 23:26:58 +0900

    [Feat ✏️] QueryProvider 생성

---
+++ src/api/query/QueryProvider.tsx
@@ -0,0 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+interface QueryProviderProps {
+  children: React.ReactNode;
+}
+
+const QueryProvider = ({ children }: QueryProviderProps) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
