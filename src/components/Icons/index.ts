a001824c7a353cd6ddae483df58bdced81890ed6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-28 14:41:19 +0900

    [Feat ✏️] HorizontalRuleIcon 생성

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,3 +2,4 @@ export { default as WriteIcon } from "./WriteIcon/WriteIcon";
 export { default as HomeIcon } from "./HomeIcon/HomeIcon";
 export { default as SearchIcon } from "./SearchIcon/SearchIcon";
 export { default as CloseIcon } from "./CloseIcon/CloseIcon";
+export { default as HorizontalRuleIcon } from "./HorizontalRuleIcon/HorizontalRuleIcon";
