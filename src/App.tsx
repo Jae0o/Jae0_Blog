9061281d0f5cdd5d5adef5b2939dac049755119d
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-29 01:42:05 +0900

    [Refactoring ⚙️] router 파일 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
 import React, { Suspense } from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
 import { QueryProvider } from "@/api";
 
 import { ContextAuthUserProvider } from "./Context/ContextAuthUser";
 import { ContextIsLoadingProvider } from "./Context/ContextIsLoading";
 import { ContextPostsProvider } from "./Context/ContextPosts";
+import { router } from "./router";
 
 const LoadingPage = React.lazy(() => import("./pages/Loading/Loading"));
-const Edit = React.lazy(() => import("./pages/Edit/Edit"));
-const Home = React.lazy(() => import("./pages/Home/Home"));
-const Post = React.lazy(() => import("./pages/Post/Post"));
-const PostDetail = React.lazy(
-  () => import("./pages/Post/components/PostDetail/PostDetail"),
-);
-const PostList = React.lazy(
-  () => import("./pages/Post/components/PostList/PostList"),
-);
-const Root = React.lazy(() => import("./Root"));
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <h1>임시 에러창</h1>,
-    children: [
-      { index: true, element: <Home /> },
-      {
-        path: "/editor/:category/:id",
-        element: <Edit />,
-      },
-      {
-        path: "/post",
-        element: <Post />,
-        children: [
-          { path: "/post/list/:category", element: <PostList /> },
-          { path: "/post/detail/:category/:id", element: <PostDetail /> },
-        ],
-      },
-    ],
-  },
-]);
 
 const App = (): React.ReactNode => {
   return (
