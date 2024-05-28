546db84046138c2dc619cb578c140df36882db17
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-29 01:47:03 +0900

    [Change 🚜 ] 코드 정리

--- src/router/route/route.tsx
+++ src/router/route/route.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 const Edit = React.lazy(() => import("@/pages/Edit/Edit"));
+
 const Home = React.lazy(() => import("@/pages/Home/Home"));
+
 const Post = React.lazy(() => import("@/pages/Post/Post"));
+
 const PostDetail = React.lazy(
   () => import("@/pages/Post/components/PostDetail/PostDetail"),
 );
+
 const PostList = React.lazy(
   () => import("@/pages/Post/components/PostList/PostList"),
 );
+
 const Root = React.lazy(() => import("@/Root"));
 
 const router = createBrowserRouter([
