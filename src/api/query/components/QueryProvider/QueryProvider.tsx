d6a90713354e58912b8ad2866064bea5ee179755
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-29 02:14:36 +0900

    [Refactoring ⚙️] QueryClient defaultOption 추가

--- src/api/query/components/QueryProvider/QueryProvider.tsx
+++ src/api/query/components/QueryProvider/QueryProvider.tsx
@@ -6,7 +6,16 @@ interface QueryProviderProps {
 }
 
 const QueryProvider = ({ children }: QueryProviderProps) => {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+        refetchOnWindowFocus: false,
+        staleTime: 1000 * 60 * 60,
+        gcTime: 1000 * 60 * 60 * 2,
+      },
+    },
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
