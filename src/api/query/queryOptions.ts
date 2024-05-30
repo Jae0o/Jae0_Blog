cd7d8c4cb08e58659039204a01216393fc3230a2
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-30 18:03:00 +0900

    [Feat ✏️] GET_POST_LIST_ALL queryOptions 구현

--- src/api/query/queryOptions.ts
+++ src/api/query/queryOptions.ts
@@ -1,7 +1,10 @@
-import { getOptions } from "../firebase";
+import { PostData } from "@/types/original";
+
+import { ResponsePostsList, getAllPostsList, getOptions } from "../firebase";
 
 export const QUERY_KEY = {
   GET_OPTIONS: (optionName: string) => ["options", "get", optionName],
+  GET_POST_LIST_ALL: (category: string) => ["post", "list", "get", category],
 };
 
 export const QUERY_OPTIONS = {
@@ -10,5 +13,24 @@ export const QUERY_OPTIONS = {
     queryFn: () => getOptions(optionName),
     gcTime: 1000 * 60 * 60 * 24,
     staleTime: 1000 * 60 * 60 * 18,
+  }),
+
+  GET_POST_LIST_ALL: () => ({
+    queryKey: QUERY_KEY.GET_POST_LIST_ALL("all"),
+    queryFn: () => getAllPostsList(),
+    gcTime: 1000 * 60 * 60,
+    staleTime: 1000 * 60 * 60,
+    select: (fetchedData: ResponsePostsList) => {
+      if (!fetchedData) {
+        return [];
+      }
+      const postsList: PostData[] = [];
+
+      for (const key in fetchedData) {
+        const convertedList: PostData[] = Object.values(fetchedData[key]);
+        postsList.push(...convertedList);
+      }
+      return postsList;
+    },
   }),
 };
