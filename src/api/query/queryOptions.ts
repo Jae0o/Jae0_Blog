7a6c051b7219bfb8633edd28bac45e7f4b47f6c7
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-05-30 21:53:16 +0900

    [Feat ✏️] GET_POST_LIST 옵션 정의

--- src/api/query/queryOptions.ts
+++ src/api/query/queryOptions.ts
@@ -1,10 +1,16 @@
 import { PostData } from "@/types/original";
 
-import { ResponsePostsList, getAllPostsList, getOptions } from "../firebase";
+import {
+  ResponsePostsList,
+  getAllPostsList,
+  getOptions,
+  getPostsList,
+} from "../firebase";
 
 export const QUERY_KEY = {
   GET_OPTIONS: (optionName: string) => ["options", "get", optionName],
   GET_POST_LIST_ALL: (category: string) => ["post", "list", "get", category],
+  GET_POST_LIST: (category: string) => ["post", "list", "get", category],
 };
 
 export const QUERY_OPTIONS = {
@@ -30,7 +36,28 @@ export const QUERY_OPTIONS = {
         const convertedList: PostData[] = Object.values(fetchedData[key]);
         postsList.push(...convertedList);
       }
-      return postsList;
+
+      return postsList.sort((prevPost, nextPost) => {
+        const prevPostDate = new Date(JSON.parse(prevPost.createAt)).getTime();
+        const nextPostDate = new Date(JSON.parse(nextPost.createAt)).getTime();
+
+        return nextPostDate - prevPostDate;
+      });
+    },
+  }),
+
+  GET_POST_LIST: (category: string) => ({
+    queryKey: QUERY_KEY.GET_POST_LIST(category),
+    queryFn: () => getPostsList(category),
+    gcTime: 1000 * 60 * 60,
+    staleTime: 1000 * 60 * 60,
+    select: (list: PostData[]) => {
+      return list.sort((prevPost, nextPost) => {
+        const prevPostDate = new Date(JSON.parse(prevPost.createAt)).getTime();
+        const nextPostDate = new Date(JSON.parse(nextPost.createAt)).getTime();
+
+        return nextPostDate - prevPostDate;
+      });
     },
   }),
 };
