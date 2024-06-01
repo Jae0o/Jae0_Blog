747500fceee81a518032d7ac59ddc44e0ed2e18b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-02 01:00:42 +0900

    [Change 🚜 ] PostList 태그 변경

--- src/pages/Post/components/PostList/components/PostListItem/PostListItem.tsx
+++ src/pages/Post/components/PostList/components/PostListItem/PostListItem.tsx
@@ -34,7 +34,7 @@ const PostListItem = ({ post }: PostListItemProps): React.ReactNode => {
       </div>
 
       <div className="ptitem__info-container">
-        <h4 className="ptitem__title"> {post.title}</h4>
+        <h3 className="ptitem__title"> {post.title}</h3>
         <div className="ptitem__category">{post.category}</div>
         <ul className="ptitem__tags">
           {post.tag &&
