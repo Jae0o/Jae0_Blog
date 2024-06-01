decf851940958225592e8afa8ae81da76d2c3b18
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-02 02:46:13 +0900

    [Feat ✏️] theme 지정

---
+++ src/styles/theme.ts
@@ -0,0 +1,113 @@
+import { DefaultTheme, css } from "styled-components";
+
+const colors = {
+  black: "#323133",
+  light_black: "#434a54",
+
+  red: "#da4453",
+  light_red: "#ed5565",
+
+  yellow: "#ffce54",
+
+  green: "#a0d468",
+  dark_green: "#8cc152",
+
+  white: "#fafafa",
+  light_white: "#e6e9ed",
+
+  grey: "#ccd1d9",
+  dark_gray: "#656d78",
+
+  text: "#323133",
+  text_point: "#ffce54",
+};
+
+const fontSizes = {
+  small: "1rem",
+  semiSmall: "1.2rem",
+  medium: "1.4rem",
+  semiLarge: "1.6rem",
+  large: "2rem",
+  big: "2.4rem",
+};
+
+const radius = {
+  size4: "0.4rem",
+  size8: "0.8rem",
+  size12: "1.2rem",
+  size16: "1.6rem",
+  size20: "2rem",
+  circle: "50%",
+};
+
+const fontWeight = {
+  regular: 400,
+  semiBold: 500,
+  bold: 600,
+  semiBolder: 700,
+  bolder: 800,
+};
+
+const zIndex = {
+  outlet: 0,
+  aside: 10,
+  nav_background: 10,
+  nav_menubar: 11,
+
+  modal: 500,
+};
+
+const scrollBarNone = css`
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+const transition = {
+  normal: `0.3s`,
+};
+
+const opacity = {
+  low: 0.3,
+  normal: 0.5,
+  high: 0.7,
+};
+
+const shadow = {
+  medium: "0px 0px 10px 2px rgba(0, 0, 0, 0.2)",
+  small: "0px 0px 5px 1px rgba(0, 0, 0, 0.2)",
+};
+
+const fontFamily = {
+  defaultFont: `"Pretendard Variable", "Pretendard", "-apple-system",
+  "BlinkMacSystemFont", system-ui, "Roboto", "Helvetica Neue", "Segoe UI",
+  "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji",
+  "Segoe UI Emoji", "Segoe UI Symbol", sans-serif`,
+};
+
+export type ColorsType = typeof colors;
+export type FontWeightType = typeof fontWeight;
+export type SizeType = typeof fontSizes;
+export type Radius = typeof radius;
+export type ZIndex = typeof zIndex;
+export type ScrollBarNone = typeof scrollBarNone;
+export type Transition = typeof transition;
+export type Opacity = typeof opacity;
+export type FontFamily = typeof fontFamily;
+export type Shadow = typeof shadow;
+
+export const theme: DefaultTheme = {
+  colors,
+  fontWeight,
+  fontSizes,
+  radius,
+  zIndex,
+  scrollBarNone,
+  opacity,
+  transition,
+  fontFamily,
+  shadow,
+};
