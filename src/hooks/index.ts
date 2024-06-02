1b7921a53c78b82cc007a83d7e721eaef3d72fbe
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-02 19:22:12 +0900

    [Refactoring ⚙️] useMediaQuery 재사용있게 변경

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,4 @@
 export { default as useHover } from "./useHover/useHover";
 export { default as useModal } from "./useModal/useModal";
 export { default as useBanner } from "./useBanner/useBanner";
-export { default as useTabletMedia } from "./useTabletMedia/useTabletMedia";
+export { default as useMediaQuery } from "./useMediaQuery/useMediaQuery";
