1ff8d269892c73c6a1dc6359623834ff98afa5d4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-02 19:15:53 +0900

    [Feat ✏️] useTabletMedia 구현

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useHover } from "./useHover/useHover";
 export { default as useModal } from "./useModal/useModal";
 export { default as useBanner } from "./useBanner/useBanner";
+export { default as useTabletMedia } from "./useTabletMedia/useTabletMedia";
