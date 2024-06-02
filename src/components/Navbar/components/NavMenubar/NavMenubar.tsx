e805f02c7951b875a5faa52976afce5a3d8eaaab
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-02 20:48:37 +0900

    [Feat ✏️] useClickAway 생성 및 반영

--- src/components/Navbar/components/NavMenubar/NavMenubar.tsx
+++ src/components/Navbar/components/NavMenubar/NavMenubar.tsx
@@ -1,51 +1,25 @@
 import { AnimatePresence } from "framer-motion";
 
-import { useEffect, useRef } from "react";
-
 import { MenubarItem } from "@/components";
 import { MENUBAR_LIST } from "@/constants";
+import { useClickAway } from "@/hooks";
 
 import * as S from "./NavMenubar.style";
 import { NavMenubarFooter } from "./components";
 
 interface NavMenubarProps {
   isToggle: boolean;
-  onToggle: () => void;
+  onClose: () => void;
 }
 
-const NavMenubar = ({ isToggle, onToggle }: NavMenubarProps) => {
-  const menubarRef = useRef<HTMLUListElement>(null);
-
-  useEffect(() => {
-    const handleAwayClick = ({ currentTarget, target }: MouseEvent) => {
-      const { current } = menubarRef;
-
-      if (!current || !isToggle || currentTarget === current) {
-        return;
-      }
-
-      if (
-        target instanceof HTMLElement &&
-        target.className === "navbar_dropdown_checkbox"
-      ) {
-        return;
-      }
-
-      onToggle();
-    };
-
-    document.addEventListener("mouseup", handleAwayClick);
-
-    return () => {
-      document.removeEventListener("mouseup", handleAwayClick);
-    };
-  }, [isToggle, onToggle]);
+const NavMenubar = ({ isToggle, onClose }: NavMenubarProps) => {
+  const ref = useClickAway<HTMLUListElement>(() => onClose());
 
   return (
     <AnimatePresence>
       {isToggle && (
         <S.NavMenubar
-          ref={menubarRef}
+          ref={ref}
           initial={{
             translateX: "110%",
             opacity: 0,
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useHover } from "./useHover/useHover";
 export { default as useModal } from "./useModal/useModal";
 export { default as useBanner } from "./useBanner/useBanner";
 export { default as useMediaQuery } from "./useMediaQuery/useMediaQuery";
+export { default as useClickAway } from "./useClickAway/useClickAway";
