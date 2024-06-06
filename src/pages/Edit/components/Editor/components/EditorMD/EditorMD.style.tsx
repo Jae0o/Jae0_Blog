6f0d4be44803b6d409e1813be21ad51bb398cbea
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-06 22:30:19 +0900

    [Design 🎨] styled 적용

---
+++ src/pages/Edit/components/Editor/components/EditorMD/EditorMD.style.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const EditorImageInput = styled.input`
+  display: none;
+`;
+
+export const EditorLayout = styled.div`
+  flex-grow: 1;
+  height: 100%;
+  margin: 0rem 1rem;
+
+  overflow-y: scroll;
+`;
