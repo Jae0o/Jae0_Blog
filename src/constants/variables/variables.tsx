555a317e90100c25d7ac1f9a3dbeff7d7613f3c1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-07 01:54:41 +0900

    [Feat ✏️] 정적 배너 에셋 및 배너 상수 생성

--- src/constants/variables/variables.tsx
+++ src/constants/variables/variables.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import BLOG_THUMBNAIL from "@/assets/banner/BLOG_THUMBNAIL.webp";
+import MASIL_THUMBNAIL from "@/assets/banner/MASIL_THUMBNAIL.webp";
+import YAP_THUMBNAIL from "@/assets/banner/YAP_THUMBNAIL.webp";
 import { Advice, PostData } from "@/types/original";
 
 import { CONTACT_ME_URL } from "../url/url";
@@ -113,4 +116,31 @@ export const MENUBAR_LIST = [
   },
   { title: "회고 기록", category: "WORK", IconComponent: <BrownBookIcon /> },
   { title: "Frontend", category: "Frontend", IconComponent: <DisplayIcon /> },
+];
+
+export const BANNER_ITEM_LIST = [
+  {
+    thumbnail: BLOG_THUMBNAIL,
+    title: "블로그 제작 과정",
+    description:
+      "평소 구현하고 싶었던 기능을 마음껏 자유롭게 구현할 수 있는 나만의 도화지 같은 공간입니다.",
+    stacks: ["React", "TypeScript", "Firebase", "Framer-motion"],
+    isRight: false,
+  },
+  {
+    thumbnail: MASIL_THUMBNAIL,
+    title: "마실가실",
+    description:
+      "잠깐의 여유와 운동이 필요한 현대인들에게 산책을 기록하고 산책로를 공유하며 색다른 마실 경험을 느낄 수 있는 서비스입니다.",
+    stacks: ["Next", "TypeScript", "Tailwind", "Storybook", "Kakao Map"],
+    isRight: true,
+  },
+  {
+    thumbnail: YAP_THUMBNAIL,
+    title: "Yap!",
+    description:
+      "Yap!은 누구에게나 열려있는 가벼운 분위기의 음악 공유 플랫폼입니다.",
+    stacks: ["React", "TypeScript", "Styled-Component", "React Player"],
+    isRight: false,
+  },
 ];
