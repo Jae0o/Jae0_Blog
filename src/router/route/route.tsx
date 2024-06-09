bdd954408b10ece86a564809c9f2f23838c23824
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-09 21:44:24 +0900

    [Feat ✏️] Error 페이지 적용

--- src/router/route/route.tsx
+++ src/router/route/route.tsx
@@ -1,27 +1,23 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 
+const Root = React.lazy(() => import("@/Root"));
+const Error = React.lazy(() => import("@/pages/Error/Error"));
 const Edit = React.lazy(() => import("@/pages/Edit/Edit"));
-
 const Home = React.lazy(() => import("@/pages/Home/Home"));
-
 const Post = React.lazy(() => import("@/pages/Post/Post"));
-
 const PostDetail = React.lazy(
   () => import("@/pages/Post/components/PostDetail/PostDetail"),
 );
-
 const PostList = React.lazy(
   () => import("@/pages/Post/components/PostList/PostList"),
 );
-
-const Root = React.lazy(() => import("@/Root"));
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    errorElement: <h1>임시 에러창</h1>,
+    errorElement: <Error />,
     children: [
       { index: true, element: <Home /> },
       {
