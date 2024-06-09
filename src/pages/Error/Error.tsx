d5b42bcb17a1b372ffc1b2c457acd8b72e012589
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-09 21:49:07 +0900

    [Feat ✏️] 등장 애니메이션 추가

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -12,7 +12,11 @@ const ErrorPage = () => {
   return (
     <S.ErrorPageBackground>
       <S.ErrorPageLayout>
-        <S.ErrorPageContainer>
+        <S.ErrorPageContainer
+          initial={{ scale: 0, opacity: 0, rotate: "-45deg" }}
+          animate={{ scale: 1, opacity: 1, rotate: 0 }}
+          transition={{ type: "spring", duration: 1.2 }}
+        >
           <S.ErrorPageTitle>페이지에 문제가 있어요!</S.ErrorPageTitle>
           <S.ErrorPageMessage>이게바로.. 404 ..?</S.ErrorPageMessage>
 
