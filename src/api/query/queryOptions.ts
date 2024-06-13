6b41908df1f64c39a412e63ab52d3c494eff1250
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-13 20:13:33 +0900

    [Remove 🗑️] 미사용 key 제거

--- src/api/query/queryOptions.ts
+++ src/api/query/queryOptions.ts
@@ -1,9 +1,8 @@
 import { PostData } from "@/types/original";
 
-import { getOptions, getPost, getPostsList } from "../firebase";
+import { getPost, getPostsList } from "../firebase";
 
 export const QUERY_KEY = {
-  GET_OPTIONS: (optionName: string) => ["options", "get", optionName],
   GET_POST_LIST_ALL: () => ["post", "list", "get", "all"],
   GET_POST_LIST: (category: string) => ["post", "list", "get", category],
   GET_POST: ({ category, id }: { category: string; id: string }) => [
@@ -15,13 +14,6 @@ export const QUERY_KEY = {
 };
 
 export const QUERY_OPTIONS = {
-  GET_OPTIONS: (optionName: string) => ({
-    queryKey: QUERY_KEY.GET_OPTIONS(optionName),
-    queryFn: () => getOptions(optionName),
-    gcTime: 1000 * 60 * 60 * 24,
-    staleTime: 1000 * 60 * 60 * 18,
-  }),
-
   GET_POST_LIST: (category: string) => ({
     queryKey: QUERY_KEY.GET_POST_LIST(category),
     queryFn: () => getPostsList({ category }),
