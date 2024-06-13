7309fb9b2ff8f630ff40ec13b415d2f163863f57
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-13 21:00:29 +0900

    [Refactoring ⚙️] edit zustand persist 적용

--- src/pages/Edit/Edit.tsx
+++ src/pages/Edit/Edit.tsx
@@ -1,61 +1,20 @@
 import "./Edit.style.css";
 
-import { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-import { MUTATION_OPTIONS, QUERY_OPTIONS } from "@/api";
-import { AlertModal, CheckAdmin } from "@/components";
+import { MUTATION_OPTIONS } from "@/api";
+import { CheckAdmin } from "@/components";
 import { QUERY_ERROR } from "@/constants";
-import { useModal } from "@/hooks";
 import { PostData } from "@/types/original";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import LoadingPage from "../Loading/Loading";
 import { Editor, EditorSub } from "./components";
+import { useCategoryList, useTagList } from "./hooks";
 
 const Edit = () => {
-  const {
-    isShowModal: isShowTagModal,
-    openModal: openTagModal,
-    closeModal: closeTagModal,
-  } = useModal();
-  const {
-    isShowModal: isShowCategoryModal,
-    openModal: openCategoryModal,
-    closeModal: closeCategoryModal,
-  } = useModal();
-
-  const {
-    data: tagList,
-    refetch: tagListRefetch,
-    isLoading: isTagListLoading,
-    isError: isTagListError,
-  } = useQuery(QUERY_OPTIONS.GET_OPTIONS("tag"));
-  const {
-    data: categoryList,
-    refetch: categoryListRefetch,
-    isLoading: isCategoryListLoading,
-    isError: isCategoryListError,
-  } = useQuery(QUERY_OPTIONS.GET_OPTIONS("category"));
-
-  useEffect(() => {
-    if (isTagListError) {
-      openTagModal();
-      tagListRefetch();
-    }
-
-    if (isCategoryListError) {
-      openCategoryModal();
-      categoryListRefetch();
-    }
-  }, [
-    categoryListRefetch,
-    isCategoryListError,
-    isTagListError,
-    openCategoryModal,
-    openTagModal,
-    tagListRefetch,
-  ]);
+  const categories = useCategoryList();
+  const tags = useTagList();
 
   const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation(MUTATION_OPTIONS.SET_POST());
@@ -72,37 +31,26 @@ const Edit = () => {
     });
   };
 
-  if (isPending || isTagListError || isTagListLoading || !tagList) {
+  if (isPending) {
     return <LoadingPage />;
   }
 
-  if (isCategoryListError || isCategoryListLoading || !categoryList) {
-    return <LoadingPage />;
-  }
+  // if (isCategoryListError || isCategoryListLoading || !categoryList) {
+  //   return <LoadingPage />;
+  // }
 
   return (
     <section className="outlet__edit">
       <CheckAdmin>
         <Editor
           id={postId}
-          categoryList={categoryList}
-          tagList={tagList}
+          categoryList={categories}
+          tagList={tags}
           onSubmit={onUploadPost}
         />
 
         <EditorSub />
       </CheckAdmin>
-
-      <AlertModal
-        isShow={isShowTagModal}
-        onClose={closeTagModal}
-        message={QUERY_ERROR.TAG_UPDATE_LIST}
-      />
-      <AlertModal
-        isShow={isShowCategoryModal}
-        onClose={closeCategoryModal}
-        message={QUERY_ERROR.CATEGORY_UPDATE_LIST}
-      />
     </section>
   );
 };
