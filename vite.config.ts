a1ee0b81e9a75e70917dd95bb2b0cf8e60658282
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-06-16 23:52:49 +0900

    [Remove 🗑️] 미사용 주석 제거

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
