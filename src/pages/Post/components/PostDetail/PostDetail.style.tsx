bc0af3486b41b3408a0e76e2ffb37691beeaee16
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-02 19:36:25 +0900

    [Refactoring ⚙️] PostDetailContainer 반영

--- src/pages/Post/components/PostDetail/PostDetail.style.tsx
+++ src/pages/Post/components/PostDetail/PostDetail.style.tsx
@@ -10,33 +10,11 @@ export const PostDetailLayout = styled.article`
   border-radius: 1.2rem;
 `;
 
-export const PostDetailContainer = styled.div`
+export const PostDetailLoading = styled.div`
   width: 100%;
   flex-grow: 1;
 
-  overflow: scroll;
-
-  ${({ theme }) => theme.scrollBarNone}
-`;
-
-export const PostDetailContent = styled.div`
-  width: 100%;
-  padding: 4rem;
-
   display: flex;
-  flex-direction: column;
-
-  overflow: scroll;
-  ${({ theme }) => theme.scrollBarNone}
-
-  user-select: none;
-`;
-
-export const PostDetailDivider = styled.span`
-  width: 100%;
-  height: 0.4rem;
-  margin: 4rem 0rem;
-
-  background-color: ${({ theme }) => theme.colors.gray};
-  border-radius: 99rem;
+  justify-content: center;
+  align-items: center;
 `;
--- src/pages/Post/components/PostDetail/PostDetail.tsx
+++ src/pages/Post/components/PostDetail/PostDetail.tsx
@@ -1,32 +1,23 @@
-import { useScroll } from "framer-motion";
-
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-import { ContextAuthUser } from "@/Context/ContextAuthUser";
 import { QUERY_OPTIONS } from "@/api";
-import {
-  AlertModal,
-  PostBanner,
-  PostBannerDecoration,
-  ProgressBar,
-} from "@/components";
+import { AlertModal, LoadingSpinner, PostBannerDecoration } from "@/components";
 import { QUERY_ERROR } from "@/constants";
 import { useModal } from "@/hooks";
 import { useQuery } from "@tanstack/react-query";
 
 import * as S from "./PostDetail.style";
-import { PostAuthAction, PostDetailInfo, PostDetailViewer } from "./components";
+import { PostDetailContainer } from "./components";
 
 const PostDetail = (): React.ReactNode => {
   const { isShowModal, openModal, closeModal } = useModal();
   const { id = "" } = useParams();
-  const { data, isError } = useQuery(QUERY_OPTIONS.GET_POST({ postId: id }));
-  const { isAuthUser } = useContext(ContextAuthUser);
-  const navigate = useNavigate();
+  const { data, isError, isLoading } = useQuery(
+    QUERY_OPTIONS.GET_POST({ postId: id }),
+  );
 
-  const ref = useRef(null);
-  const { scrollYProgress } = useScroll({ container: ref });
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (isError) {
@@ -36,37 +27,22 @@ const PostDetail = (): React.ReactNode => {
 
   return (
     <>
-      {data && (
-        <S.PostDetailLayout>
-          <PostBannerDecoration />
-          <S.PostDetailContainer ref={ref}>
-            <ProgressBar scrollYProgress={scrollYProgress} />
-            <PostBanner
-              thumbnail={data.thumbnail}
-              mainText={data.title}
-              height={50}
-              objectFit={"contain"}
-            />
+      <S.PostDetailLayout>
+        <PostBannerDecoration />
 
-            <S.PostDetailContent data-color-mode="light">
-              <PostDetailInfo post={data} />
+        {isLoading && (
+          <S.PostDetailLoading>
+            <LoadingSpinner size="24rem" />
+          </S.PostDetailLoading>
+        )}
 
-              <S.PostDetailDivider />
-
-              <PostDetailViewer content={data.main} />
-
-              <S.PostDetailDivider />
-
-              {isAuthUser && (
-                <>
-                  <PostAuthAction postId={id} />
-                  <S.PostDetailDivider />
-                </>
-              )}
-            </S.PostDetailContent>
-          </S.PostDetailContainer>
-        </S.PostDetailLayout>
-      )}
+        {data && (
+          <PostDetailContainer
+            id={id}
+            postData={data}
+          />
+        )}
+      </S.PostDetailLayout>
 
       <AlertModal
         isShow={isShowModal}
