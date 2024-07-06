727f66242a966203fe600f44ba4a14e5bca9bc73
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-06 16:25:48 +0900

    [Feat ✏️] AuthProvider 구현

--- src/api/components/AuthProvider/AuthProvider.tsx
+++ src/api/components/AuthProvider/AuthProvider.tsx
@@ -1,5 +1,42 @@
-export const AuthProvider = () => {
-  return <div>AuthProvider</div>;
+import { onAuthStateChanged } from "firebase/auth";
+
+import { useEffect } from "react";
+
+import { auth } from "@/api/firebase";
+import { useAuth } from "@/stores";
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const { VITE_FIREBASE_ADMIN_USER_ID } = import.meta.env;
+
+  const { setIsLoggedIn, setAuthUserId, setIsAuthUser } = useAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (!user) {
+        setIsAuthUser(false);
+        setIsLoggedIn(false);
+        setAuthUserId("");
+        return;
+      }
+
+      setIsLoggedIn(true);
+      setAuthUserId(user.uid);
+
+      if (VITE_FIREBASE_ADMIN_USER_ID === user.uid) {
+        setIsAuthUser(true);
+      }
+    });
+  }, [
+    VITE_FIREBASE_ADMIN_USER_ID,
+    setAuthUserId,
+    setIsAuthUser,
+    setIsLoggedIn,
+  ]);
+
+  return <>{children}</>;
 };
 
 export default AuthProvider;
