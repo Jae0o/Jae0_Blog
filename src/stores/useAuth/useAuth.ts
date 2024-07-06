3bda7365757dd72eb698f82fc2a4ee693be67d06
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-06 16:26:34 +0900

    [Feat ✏️] useAuth setter 구현

--- src/stores/useAuth/useAuth.ts
+++ src/stores/useAuth/useAuth.ts
@@ -4,12 +4,20 @@ interface UseAuthStore {
   isLoggedIn: boolean;
   isAuthUser: boolean;
   authUserId: string;
+
+  setIsLoggedIn: (value: boolean) => void;
+  setIsAuthUser: (value: boolean) => void;
+  setAuthUserId: (id: string) => void;
 }
 
-const useAuth = create<UseAuthStore>()(() => ({
+const useAuth = create<UseAuthStore>()(set => ({
   isAuthUser: false,
   isLoggedIn: false,
   authUserId: "",
+
+  setIsLoggedIn: value => set(() => ({ isLoggedIn: value })),
+  setIsAuthUser: value => set(() => ({ isAuthUser: value })),
+  setAuthUserId: id => set(() => ({ authUserId: id })),
 }));
 
 export default useAuth;
