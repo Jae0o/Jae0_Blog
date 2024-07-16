7b83e9afd3ab61f900ac0671514d12bfb0454431
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-07-16 22:58:43 +0900

    [Feat ✏️] visitorCheck 로직 구현

--- src/api/components/VisitorsCheckerProvider/VisitorsCheckerProvider.tsx
+++ src/api/components/VisitorsCheckerProvider/VisitorsCheckerProvider.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 
 import { MUTATION_OPTIONS } from "@/api/query";
+import { useLocalStorage } from "@/hooks";
 import { useMutation } from "@tanstack/react-query";
 
 interface VisitorsCheckerProviderProps {
@@ -10,9 +11,26 @@ interface VisitorsCheckerProviderProps {
 const VisitorsCheckerProvider = ({
   children,
 }: VisitorsCheckerProviderProps) => {
+  const { data, setLocalStorage } = useLocalStorage({
+    key: "visitCheckTime",
+    initialData: "",
+  });
   const { mutate } = useMutation(MUTATION_OPTIONS.INCREASE_VISIT_COUNT());
 
-  useEffect(() => {}, [mutate]);
+  useEffect(() => {
+    const time = new Date();
+    const currentDate = `${time.getFullYear()}/${time.getMonth() + 1}/${time.getDate()}`;
+
+    if (currentDate === data) {
+      return;
+    }
+
+    setLocalStorage(currentDate);
+    mutate();
+
+    // 첫 렌더링 시 동작을 위해 디펜던시 제외
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return <>{children}</>;
 };
