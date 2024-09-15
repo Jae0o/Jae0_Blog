cc770935aee68ac96c7f649e7e5b9e0c10b00784
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-09-15 11:57:26 +0900

    [Feat ✏️] store 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
+import * as S from "./App.style";
+import theme from "./styles/theme";
 import { ThemeProvider } from "styled-components";
 
 import React, { Suspense } from "react";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
 import { AuthProvider, QueryProvider, VisitorsCheckerProvider } from "@/api";
 
-import * as S from "./App.style";
 import { router } from "./router";
-import GlobalStyles from "./styles/GlobalStyles";
-import theme from "./styles/theme";
+import { store } from "./stores";
 
 const InitialLoading = React.lazy(
   () => import("./pages/InitialLoading/InitialLoading"),
@@ -18,15 +21,17 @@ const App = (): React.ReactNode => {
   return (
     <Suspense fallback={<InitialLoading />}>
       <QueryProvider>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles />
-          <AuthProvider>
-            <VisitorsCheckerProvider>
-              <S.ModalPlace id="modal" />
-              <RouterProvider router={router} />
-            </VisitorsCheckerProvider>
-          </AuthProvider>
-        </ThemeProvider>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <AuthProvider>
+              <VisitorsCheckerProvider>
+                <S.ModalPlace id="modal" />
+                <RouterProvider router={router} />
+              </VisitorsCheckerProvider>
+            </AuthProvider>
+          </ThemeProvider>
+        </Provider>
       </QueryProvider>
     </Suspense>
   );
