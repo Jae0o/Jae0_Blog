f9ce9a435e030de793872a5ff6b4f2644d6c0848
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-09-18 12:12:32 +0900

    [Remove 🗑️] useAuthStore 제거

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,4 +1,3 @@
 export { default as useOptionsStore } from "./useOptionsStore/useOptionsStore";
-export { default as useAuthStore } from "./useAuthStore/useAuthStore";
 
 export * from "./store/store";
