261fa351c35d0d10f1a877733f022d44461e2ba6
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-09-28 09:39:09 +0900

    [Refactoring ⚙️] useToggle 대체 - PostDetail Page

--- src/pages/Post/components/PostDetail/PostDetail.tsx
+++ src/pages/Post/components/PostDetail/PostDetail.tsx
@@ -1,17 +1,18 @@
+import * as S from "./PostDetail.style";
+
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { QUERY_OPTIONS } from "@/api";
 import { AlertModal, LoadingSpinner, PostBannerDecoration } from "@/components";
 import { QUERY_ERROR } from "@/constants";
-import { useModal } from "@/hooks";
+import { useToggle } from "@/hooks";
 import { useQuery } from "@tanstack/react-query";
 
-import * as S from "./PostDetail.style";
 import { PostDetailContainer } from "./components";
 
 const PostDetail = (): React.ReactNode => {
-  const { isShowModal, openModal, closeModal } = useModal();
+  const { isToggle, handleOnToggle, handleOffToggle } = useToggle();
   const { id = "" } = useParams();
   const { data, isError, isLoading } = useQuery(
     QUERY_OPTIONS.GET_POST({ postId: id }),
@@ -21,9 +22,9 @@ const PostDetail = (): React.ReactNode => {
 
   useEffect(() => {
     if (isError) {
-      openModal();
+      handleOnToggle();
     }
-  }, [isError, openModal]);
+  }, [isError, handleOnToggle]);
 
   return (
     <>
@@ -45,9 +46,9 @@ const PostDetail = (): React.ReactNode => {
       </S.PostDetailLayout>
 
       <AlertModal
-        isShow={isShowModal}
+        isShow={isToggle}
         onClose={() => {
-          closeModal();
+          handleOffToggle();
           navigate("/");
         }}
         message={QUERY_ERROR.GET_POST}
