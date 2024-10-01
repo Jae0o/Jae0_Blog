b8cd870646683b87f32052ecf9cc048f7756d2f3
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-10-01 09:42:35 +0900

    [Remove 🗑️] useModal ( useToggle로 대체 )

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export { default as useHover } from "./useHover/useHover";
-export { default as useModal } from "./useModal/useModal";
 export { default as useBanner } from "./useBanner/useBanner";
 export { default as useMediaQuery } from "./useMediaQuery/useMediaQuery";
 export { default as useClickAway } from "./useClickAway/useClickAway";
