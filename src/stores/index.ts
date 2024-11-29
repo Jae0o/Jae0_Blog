1d54e89b97275592bb68838a20bca9590344d8ea
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-11-29 10:41:26 +0900

    [Remove 🗑️] redux code

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,4 +1,2 @@
-export * from "./store/store";
-
 export { default as useAuthStore } from "./useAuthStore/useAuthStore";
 export { default as useOptionsStore } from "./useOptionsStore/useOptionsStore";
--- src/stores/reducer/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as authReducer } from "./auth/auth.slice";
-export * from "./auth/auth.slice";
-
-export { default as optionsReducer } from "./options/options.slice";
-export * from "./options/options.slice";
