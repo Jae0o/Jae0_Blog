730f5c5f42a1451bfce542837e711191e8fabe10
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-11-29 10:44:59 +0900

    [Remove 🗑️] Redux Code

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,26 @@ import theme from "./styles/theme";
 import { ThemeProvider } from "styled-components";
 
 import React, { Suspense } from "react";
-import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
 import { AuthProvider, QueryProvider, VisitorsCheckerProvider } from "@/api";
 
 import InitialLoading from "./pages/InitialLoading/InitialLoading";
 import { router } from "./router";
-import { persistor, store } from "./stores";
-
-import { PersistGate } from "redux-persist/integration/react";
 
 const App = (): React.ReactNode => {
   return (
     <Suspense fallback={<InitialLoading />}>
       <QueryProvider>
-        <Provider store={store}>
-          <PersistGate
-            loading={<InitialLoading />}
-            persistor={persistor}
-          >
-            <ThemeProvider theme={theme}>
-              <GlobalStyles />
-              <AuthProvider>
-                <VisitorsCheckerProvider>
-                  <S.ModalPlace id="modal" />
-                  <RouterProvider router={router} />
-                </VisitorsCheckerProvider>
-              </AuthProvider>
-            </ThemeProvider>
-          </PersistGate>
-        </Provider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <AuthProvider>
+            <VisitorsCheckerProvider>
+              <S.ModalPlace id="modal" />
+              <RouterProvider router={router} />
+            </VisitorsCheckerProvider>
+          </AuthProvider>
+        </ThemeProvider>
       </QueryProvider>
     </Suspense>
   );
