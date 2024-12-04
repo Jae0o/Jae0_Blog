dfffe68882101d9ced6d0c6a811b7e999f5fc252
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-05 08:37:20 +0900

    [Feat ✏️] HelmetProvider 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 import "./index.css";
 
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 
 import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const root = document.getElementById("root")!;
+const rootDom = ReactDOM.createRoot(root);
+
+rootDom.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>,
+);
