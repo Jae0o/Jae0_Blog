4ab49cde85bfa1a77bc13c78d412cd6eb7b72f79
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-05 09:06:25 +0900

    [Feat ✏️] meta tag constants ( Home 관련 )

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export * from "./alertMessage/alertMessage";
 export * from "./url/url";
 export * from "./variables/variables";
 export * from "./key/key";
+export * from "./metaTag/metaTag";
---
+++ src/constants/metaTag/metaTag.ts
@@ -0,0 +1,35 @@
+import BLOG_BANNER from "@/assets/banner/BLOG_THUMBNAIL.webp";
+
+export const META_SITE_NAME = "Jae0's Blog";
+
+export const META_DATA = {
+  HOME: {
+    title: META_SITE_NAME,
+    description: "FrontEnd 개발자의 나만의 도화지 Blog!",
+    keywords: [
+      "frontend",
+      "프론트엔드",
+      "dev",
+      "developer",
+      "개발자",
+      "develop",
+      "개발",
+      "react",
+      "리액트",
+      "javascript",
+      "자바스크립트",
+      "typescript",
+      "타입스크립트",
+      "next",
+      "next.js",
+      "넥스트",
+      "css",
+      "masilgasil",
+      "마실가실",
+      "Project",
+      "프로젝트",
+      "개인프로젝트",
+    ],
+    image: BLOG_BANNER,
+  },
+};
