e02161bc7a46a9ca72e06975e9216646f89c0e6b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-05 11:16:14 +0900

    🐛 : API Key 세팅 변경

--- src/api/firebase/firebase.ts
+++ src/api/firebase/firebase.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { FirebaseApp, FirebaseOptions } from "firebase/app";
 
 const {
-  VITE_FIREBASE_API_KET,
+  VITE_FIREBASE_API_KEY,
   VITE_FIREBASE_AUTH_DOMAIN,
   VITE_FIREBASE_PROJECT_ID,
   VITE_FIREBASE_STORAGE_BUCKET,
@@ -13,7 +13,7 @@ const {
 } = import.meta.env;
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: VITE_FIREBASE_API_KET,
+  apiKey: VITE_FIREBASE_API_KEY,
   authDomain: VITE_FIREBASE_AUTH_DOMAIN,
   projectId: VITE_FIREBASE_PROJECT_ID,
   storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
