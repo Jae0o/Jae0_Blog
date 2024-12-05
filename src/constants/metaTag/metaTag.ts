5cbc78f912dd1e3c5f5648e4ce2bbece9f5f1894
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-05 09:10:58 +0900

    [Change 🚜 ] Home Meta Image 변경

--- src/constants/metaTag/metaTag.ts
+++ src/constants/metaTag/metaTag.ts
@@ -1,5 +1,3 @@
-import BLOG_BANNER from "@/assets/banner/BLOG_THUMBNAIL.webp";
-
 export const META_SITE_NAME = "Jae0's Blog";
 
 export const META_DATA = {
@@ -30,6 +28,7 @@ export const META_DATA = {
       "프로젝트",
       "개인프로젝트",
     ],
-    image: BLOG_BANNER,
+    image:
+      "https://res.cloudinary.com/dya70nmfp/image/upload/c_thumb,w_200,g_face/v1720707563/%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA_2024-07-11_%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE_11.13.25_lc13ib.png",
   },
 };
