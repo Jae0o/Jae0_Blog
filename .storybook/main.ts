b921969d42e0594e39942cb3be25813a1be987b1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-06 15:42:57 +0900

    [Chore 🚀] Storybook Setting

---
+++ .storybook/main.ts
@@ -0,0 +1,36 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-styling-webpack",
+    "@storybook/addon-themes",
+  ],
+
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+
+  webpackFinal: async prevConfig => {
+    const newConfig = { ...prevConfig };
+
+    if (newConfig.resolve) {
+      newConfig.resolve.alias = {
+        ...newConfig.resolve.alias,
+        "@": path.resolve(__dirname, "../src"),
+      };
+    }
+
+    return newConfig;
+  },
+
+  staticDirs: ["..\\public"],
+};
+export default config;
---
+++ .storybook/preview.ts
@@ -0,0 +1,27 @@
+import "../src/lib/css/globals.css";
+
+import { withThemeByClassName } from "@storybook/addon-themes";
+
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: "light",
+        dark: "dark",
+      },
+      defaultTheme: "light",
+    }),
+  ],
+};
+
+export default preview;
