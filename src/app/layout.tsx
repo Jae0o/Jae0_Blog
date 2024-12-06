232aa91efe524e1fec39f7d18e3d10eea7de29ba
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-06 14:52:43 +0900

    [Chore 🚀] Prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
