b6017858f9f18b0fdc1b6228ea311313ad374b1b
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-09 15:02:15 +0900

    [Fix 🪛] ModalPortal server 문제 개선

--- src/lib/components/client/Modal/components/ModalPortal/ModalPortal.tsx
+++ src/lib/components/client/Modal/components/ModalPortal/ModalPortal.tsx
@@ -6,9 +6,15 @@ import { createPortal } from "react-dom";
 import { ModalPortalProps } from "./ModalPortal.type";
 
 const ModalPortal = ({ isShow, children }: ModalPortalProps) => {
-  const [ModalBaseElement] = useState<HTMLElement>(document.body);
+  const [ModalBaseElement, setModalBaseElement] = useState<HTMLElement | null>(
+    null,
+  );
 
   useEffect(() => {
+    setModalBaseElement(document.body);
+
+    if (!ModalBaseElement) return;
+
     if (isShow) {
       const originalStyle = window.getComputedStyle(ModalBaseElement).overflow;
       document.body.style.overflow = "hidden";
@@ -19,7 +25,7 @@ const ModalPortal = ({ isShow, children }: ModalPortalProps) => {
     }
   }, [isShow, ModalBaseElement]);
 
-  if (!isShow) {
+  if (!isShow || !ModalBaseElement) {
     return null;
   }
 
