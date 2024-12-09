6b60f76d168bb223d4c6a30d8857e097d76ce6af
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-09 10:42:21 +0900

    [Design 🎨] color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,7 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -11,6 +10,31 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+
+        black_500: "var(--black)",
+        black_300: "var(--light_black)",
+
+        red_500: "var(--red)",
+        red_300: "var(--light_red)",
+
+        yellow_500: "var(--yellow)",
+
+        green_500: "var(--green)",
+        green_700: "var(--dark_green)",
+
+        gray_500: "var(--gray)",
+        gray_700: "var(--dark_gray)",
+
+        white_500: "var(--white)",
+        white_300: "var(--light_white)",
+
+        text: "var(--text)",
+        text_point: "var(--text_point)",
+
+        transparent_10: "var(--transparent_10)",
+        transparent_30: "var(--transparent_30)",
+        transparent_50: "var(--transparent_50)",
+        transparent_90: "var(--transparent_90)",
       },
     },
   },
