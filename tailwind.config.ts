3196a04f50bee86a2f332cdfb986bbc597980c47
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-09 10:47:05 +0900

    [Design 🎨] Style theme 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,6 +36,59 @@ export default {
         transparent_50: "var(--transparent_50)",
         transparent_90: "var(--transparent_90)",
       },
+      fontSize: {
+        small: "var(--font-size-small)",
+        semiSmall: "var(--font-size-semi-small)",
+
+        medium: "var(--font-size-medium)",
+
+        semiLarge: "var(--font-size-semi-large)",
+        large: "var(--font-size-large)",
+
+        semiBig: "var(--font-size-semi-big)",
+        big: "var(--font-size-big)",
+      },
+
+      borderRadius: {
+        4: "var(--radius-4)",
+        8: "var(--radius-8)",
+        12: "var(--radius-12)",
+        16: "var(--radius-16)",
+        20: "var(--radius-20)",
+        circle: "var(--radius-circle)",
+      },
+
+      fontWeight: {
+        regular: "var(--font-weight-regular)",
+        medium: "var(--font-weight-medium)",
+        semiBold: "var(--font-weight-semi-bold)",
+        bold: "var(--font-weight-bold)",
+        semiBolder: "var(--font-weight-semi-bolder)",
+        bolder: "var(--font-weight-bolder)",
+      },
+
+      zIndex: {
+        outlet: "var(--z-index-outlet)",
+        aside: "var(--z-index-aside)",
+        navBackground: "var(--z-index-nav-background)",
+        navMenubar: "var(--z-index-nav-menubar)",
+        modal: "var(--z-index-modal)",
+      },
+
+      transitionDuration: {
+        normal: "var(--transition-normal)",
+      },
+
+      opacity: {
+        low: "var(--opacity-low)",
+        normal: "var(--opacity-normal)",
+        high: "var(--opacity-high)",
+      },
+
+      boxShadow: {
+        medium: "var(--shadow-medium)",
+        small: "var(--shadow-small)",
+      },
     },
   },
   plugins: [],
