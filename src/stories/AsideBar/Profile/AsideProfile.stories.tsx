2a33be5c820d0ddc281e315ce324a90fb073314a
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-18 14:46:51 +0900

    ✏️ Feat :  AsideProfile Storybook 생성

---
+++ src/stories/AsideBar/Profile/AsideProfile.stories.tsx
@@ -0,0 +1,33 @@
+import { AsideProfile } from "@/lib/container/server/AsideBar/components";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+/**
+ * ## AsideProfile Component
+ *
+ * AsideBar 내부 Profile 영역에서 사용되어지는 AsideProfile Component입니다. <br>
+ * 내부에는 `<AsideProfileTitle />` `<AsideProfileButton />` 컴포넌트가 사용되어집니다.
+
+ *
+ * <br>
+ *
+ * ```tsx
+ * import { AsideProfile } from "";
+ *
+ * return <AsideProfile />
+ * ```
+ * */
+const meta = {
+  title: "Components/AsideBar/Profile",
+  component: AsideProfile,
+  parameters: {
+    layout: "centered",
+  },
+
+  tags: ["autodocs"],
+} satisfies Meta<typeof AsideProfile>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
