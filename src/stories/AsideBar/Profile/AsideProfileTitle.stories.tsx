4a73ea6a8814fd4e54e4e338000706c5f99f74c4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-18 14:42:59 +0900

    ✏️ Feat :  AsideProfileTitle Storybook 생성

---
+++ src/stories/AsideBar/Profile/AsideProfileTitle.stories.tsx
@@ -0,0 +1,33 @@
+import { AsideProfileTitle } from "@/lib/container/server/AsideBar/components/AsideProfile/components";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+/**
+ * ## AsideProfileTitle Component
+ *
+ * AsideBar 내부 Profile 영역에서 사용되어지는 AsideProfileTitle Component입니다. <br>
+ * Title `click` 시 Home 으로 이동하는 이벤트가 동작합니다.
+
+ *
+ * <br>
+ *
+ * ```tsx
+ * import { AsideProfileTitle } from "";
+ *
+ * return <AsideProfileTitle />
+ * ```
+ * */
+const meta = {
+  title: "Components/AsideBar/Profile/Title",
+  component: AsideProfileTitle,
+  parameters: {
+    layout: "centered",
+  },
+
+  tags: ["autodocs"],
+} satisfies Meta<typeof AsideProfileTitle>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
