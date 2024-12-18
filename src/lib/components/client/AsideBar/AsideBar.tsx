62b65d4e581b1b93efbc43d881465104b2e5c2e1
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-18 12:25:04 +0900

    🎨 Style :  AsideBar Footer 반영 & 스타일 추가

--- src/lib/components/client/AsideBar/AsideBar.tsx
+++ src/lib/components/client/AsideBar/AsideBar.tsx
@@ -1,16 +1,23 @@
 "use client";
 
+import { AsideFooter } from "./components";
+
 import { twMerge } from "tailwind-merge";
 
 const AsideBar = () => {
   return (
     <aside
       className={twMerge(
-        "w-[24rem] h-full absolute left-0 top-0 bottom-0 bg-red-600 border-[1rem] border-yellow-400 opacity-0 animate-aside_in translate-x-[-24rem]",
+        "w-[24rem] h-full absolute left-0 top-0 bottom-0 flex items-end opacity-0 animate-aside_in translate-x-[-24rem]",
         "tablet:animate-aside_out translate-x-[0rem]",
+        "border-r-2 border-r-red-600",
       )}
     >
-      AsideBar
+      <div className="w-[22rem] h-[calc(100%-8rem)] flex flex-col absolute left-[4rem] top-0  border-2 border-blue-500">
+        Aside
+      </div>
+
+      <AsideFooter />
     </aside>
   );
 };
