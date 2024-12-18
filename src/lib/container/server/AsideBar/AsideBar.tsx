55d4f96dc714d2fc1613903d76f9f6b7f008971c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-18 14:09:41 +0900

    ✏️ Feat :  AsideProfile 생성

--- src/lib/container/server/AsideBar/AsideBar.tsx
+++ src/lib/container/server/AsideBar/AsideBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AsideFooter } from "./components";
+import { AsideFooter, AsideProfile } from "./components";
 
 import { twMerge } from "tailwind-merge";
 
@@ -8,13 +8,12 @@ const AsideBar = () => {
   return (
     <aside
       className={twMerge(
-        "w-[24rem] h-full absolute left-0 top-0 bottom-0 flex items-end opacity-0 animate-aside_in translate-x-[-24rem]",
+        "w-[24rem] h-full absolute left-0 top-0 bottom-0 flex items-end bg-yellow_500 opacity-0 animate-aside_in translate-x-[-24rem]",
         "tablet:animate-aside_out translate-x-[0rem]",
-        "border-r-2 border-r-red-600",
       )}
     >
       <div className="w-[22rem] h-[calc(100%-8rem)] flex flex-col absolute left-[4rem] top-0  border-2 border-blue-500">
-        Aside
+        <AsideProfile />
       </div>
 
       <AsideFooter />
--- src/lib/container/server/AsideBar/components/AsideProfile/AsideProfile.tsx
+++ src/lib/container/server/AsideBar/components/AsideProfile/AsideProfile.tsx
@@ -1,5 +1,37 @@
+import Image from "next/image";
+
+import { PROFILE_IMAGE } from "@/lib/assets";
+
+import { ASIDE_ACTIONS } from "../../constants";
+import { AsideProfileButton, AsideProfileTitle } from "./components";
+
 const AsideProfile = () => {
-  return <div>AsideProfile</div>;
+  return (
+    <article className="w-[22rem] h-[22rem] p-[1rem] mt-[6rem] flex flex-col justify-center items-center rounded-[1rem] bg-white_500 shadow-medium select-none">
+      <header className="w-full h-[14rem] flex">
+        <div className="w-[14rem] h-[14rem] rounded-[1rem] shadow-medium">
+          <Image
+            src={PROFILE_IMAGE}
+            alt="프로필 메인 이미지"
+            className="w-[14rem] h-[14rem] rounded-[1rem]"
+          />
+        </div>
+
+        <div className="h-full grow-[1] flex flex-col justify-evenly items-center">
+          {ASIDE_ACTIONS.map(({ action, path, Icon }) => (
+            <AsideProfileButton
+              key={action}
+              path={path}
+              action={action}
+              Icon={Icon}
+            />
+          ))}
+        </div>
+      </header>
+
+      <AsideProfileTitle />
+    </article>
+  );
 };
 
 export default AsideProfile;
