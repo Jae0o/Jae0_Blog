17b639247b33e6b6fd8be44e71b3e024db44757c
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-19 21:07:59 +0900

    ✏️ Feat :  MainFrame will-change 추가

--- src/lib/components/server/Frame/MainFrame/MainFrame.tsx
+++ src/lib/components/server/Frame/MainFrame/MainFrame.tsx
@@ -6,7 +6,7 @@ const MainFrame = ({ children }: PropsWithChildren) => {
   return (
     <main
       className={twMerge(
-        "w-[calc(100% - 24rem)] h-full animate-main_frame_aside_in",
+        "w-[calc(100% - 24rem)] h-full will-change-[height, width] animate-main_frame_aside_in",
         "tablet:w-full tablet:h-[calc(100% - 6rem)] tablet:animate-main_frame_aside_out",
         "border-[1rem] border-blue-500 bg-orange-300",
       )}
