0dc35759ebe7346b2ac1f4eef25b0235426dc415
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-20 13:42:36 +0900

    ✏️ Feat :  MainHome 컴포넌트 추가 및 기존 페이지 삭제

---
+++ src/app/(Home)/page.tsx
@@ -0,0 +1,11 @@
+import { HomeTitle } from "./_components";
+
+const MainHome = () => {
+  return (
+    <section className="w-full h-full flex flex-col items-center overflow-scroll scrollbar-hide">
+      <HomeTitle />
+    </section>
+  );
+};
+
+export default MainHome;
--- src/app/page.tsx
+++
@@ -1,5 +0,0 @@
-const MainHome = () => {
-  return <main />;
-};
-
-export default MainHome;
