fa5efc29c043dcd96566539463af781a20cc9164
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-20 15:24:50 +0900

    ✏️ Feat :  배너 이미지 모듈 추가

---
+++ src/lib/assets/banner/index.ts
@@ -0,0 +1,3 @@
+export { default as BLOG_BANNER } from "./BLOG_THUMBNAIL.webp";
+export { default as MASIL_BANNER } from "./MASIL_THUMBNAIL.webp";
+export { default as YAP_BANNER } from "./YAP_THUMBNAIL.webp";
