b35bcc7d819fc58966701484c16f91d571ce11de
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2024-12-20 13:42:22 +0900

    🎨 Style :  테스트 스타일 제거

--- src/lib/container/server/Frame/MainFrame/MainFrame.tsx
+++ src/lib/container/server/Frame/MainFrame/MainFrame.tsx
@@ -8,7 +8,6 @@ const MainFrame = ({ children }: PropsWithChildren) => {
       className={twMerge(
         "w-[calc(100% - 24rem)] h-full will-change-[height, width] animate-main_frame_aside_in",
         "tablet:w-full tablet:h-[calc(100% - 6rem)] tablet:animate-main_frame_aside_out",
-        "border-[1rem] border-blue-500 bg-orange-300",
       )}
     >
       {children}
