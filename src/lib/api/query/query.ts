726ee8f180e141cb8beb83f794c243f40759668f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-02 11:37:17 +0900

    ✏️ Feat :  QUERY_KEY 등록

--- src/lib/api/query/query.ts
+++ src/lib/api/query/query.ts
@@ -4,6 +4,16 @@ import { QueryClient } from "@tanstack/react-query";
 
 export const getQueryClient = cache(() => new QueryClient());
 
-export const QUERY_KEY = {};
+export const QUERY_KEY = {
+  POST: {
+    ALL: ["POST"],
+
+    LIST_ALL: () => [...QUERY_KEY.POST.ALL, "LIST"],
+    LIST: (category: string) => [...QUERY_KEY.POST.LIST_ALL(), category],
+
+    DETAIL_ALL: () => [...QUERY_KEY.POST.ALL, "DETAIL"],
+    DETAIL: (id: string) => [...QUERY_KEY.POST.DETAIL_ALL(), id],
+  },
+};
 
 export const QUERY_OPTIONS = {};
