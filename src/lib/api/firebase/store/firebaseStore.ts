b46c192445da85f713e80f2700b58ce920bdddd5
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-02 12:02:02 +0900

    ✏️ Feat :  모든 게시물 목록 가져오기 API 구현

--- src/lib/api/firebase/store/firebaseStore.ts
+++ src/lib/api/firebase/store/firebaseStore.ts
@@ -1,5 +1,47 @@
-import { getFirestore } from "@firebase/firestore";
+import { PostData } from "@/lib/types/PostData/PostData";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from "@firebase/firestore";
 
 import { firebaseApp } from "../firebase";
 
 export const fireStore = getFirestore(firebaseApp);
+
+export const getAllPostList = async ({ cursor }: { cursor: string }) => {
+  let queryRef;
+
+  if (cursor) {
+    queryRef = query(
+      collection(fireStore, "posts"),
+      orderBy("createdAt", "desc"),
+      startAfter(cursor),
+      limit(4),
+    );
+  } else {
+    queryRef = query(
+      collection(fireStore, "posts"),
+      orderBy("createAt", "desc"),
+      limit(4),
+    );
+  }
+
+  const res = await getDocs(queryRef).catch(() => {
+    throw Error("Failed to fetch posts list all");
+  });
+
+  const result: PostData[] = [];
+
+  res.forEach(doc => {
+    if (doc.exists()) {
+      result.push(doc.data() as PostData);
+    }
+  });
+
+  return result;
+};
