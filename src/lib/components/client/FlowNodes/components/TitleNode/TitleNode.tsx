0f56a7cb01339b3cf06c0baa1853127b1639831f
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-07 10:13:49 +0900

    🎨 Style :  TitleNode 디자인 수정

--- src/lib/components/client/FlowNodes/components/TitleNode/TitleNode.tsx
+++ src/lib/components/client/FlowNodes/components/TitleNode/TitleNode.tsx
@@ -6,8 +6,8 @@ type TitleNodeProps = Node<{ title: string }, "stack">;
 const TitleNode = ({ data }: NodeProps<TitleNodeProps>) => {
   return (
     <>
-      <div className="w-[12rem] p-[0.4rem] rounded-[0.8rem] border-[0.2rem] border-red_500">
-        <p className="w-full h-[2rem] flex items-center justify-center text-[1.2rem] font-bold">
+      <div className="w-[12rem] p-[0.4rem] rounded-[0.8rem] bg-red_500">
+        <p className="w-full h-[2rem] flex items-center justify-center text-white_500 text-[1.2rem] font-bold">
           {data.title}
         </p>
       </div>
