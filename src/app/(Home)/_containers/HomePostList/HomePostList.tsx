8af7c186d42764a53dd52028b8668e5bb12a01b4
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-07 15:59:54 +0900

    ✏️ Feat :  HomePostList 구현

--- src/app/(Home)/_containers/HomePostList/HomePostList.tsx
+++ src/app/(Home)/_containers/HomePostList/HomePostList.tsx
@@ -1,12 +1,40 @@
 "use client";
 
+import { useEffect, useRef } from "react";
+
 import { useGetAllPostList } from "@/lib/api";
 
-const HomePostList = () => {
-  const { data } = useGetAllPostList();
+import { HomeObserveBox, HomePostItem } from "./components";
 
-  console.log(data);
-  return <div>HomePostList</div>;
+import { useInView } from "motion/react";
+
+const HomePostList = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+
+  const { data, hasNextPage, isFetching, fetchNextPage } = useGetAllPostList();
+
+  useEffect(() => {
+    if (isInView && hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, hasNextPage, isFetching, isInView]);
+
+  return (
+    <ul className="w-full max-w-[90rem] p-[5rem] flex flex-wrap flex-row items-center justify-center gap-[6rem]">
+      {data &&
+        data.pages.flat().flatMap(post => (
+          <HomePostItem
+            key={post.id}
+            post={post}
+          />
+        ))}
+
+      {isFetching && <div>isFetching</div>}
+
+      <HomeObserveBox ref={ref} />
+    </ul>
+  );
 };
 
 export default HomePostList;
