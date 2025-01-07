d5bea39c5fc7880d5da216e663cd0e9761a08782
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-07 13:29:05 +0900

    ✏️ Feat :  zustand Icon 생성

--- src/lib/components/server/Icons/stacks/index.ts
+++ src/lib/components/server/Icons/stacks/index.ts
@@ -2,3 +2,4 @@ export { default as NextJSIcon } from "./NextJSIcon/NextJSIcon";
 export { default as TailwindIcon } from "./TailwindIcon/TailwindIcon";
 export { default as TanstackIcon } from "./TanstackIcon/TanstackIcon";
 export { default as FirebaseIcon } from "./FirebaseIcon/FirebaseIcon";
+export { default as ZustandIcon } from "./ZustandIcon/ZustandIcon";
