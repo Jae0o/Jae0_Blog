6f6953d93b63962951f2601f214286b585c7eb86
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-07 09:10:38 +0900

    ♻️ Refactor :  STACK 상수 ID 값 수정

--- src/app/introduce/_components/IntroFlow/constants/STACK/STACK.tsx
+++ src/app/introduce/_components/IntroFlow/constants/STACK/STACK.tsx
@@ -6,21 +6,21 @@ import {
 
 export const STACK_FLOW_NODES = [
   {
-    id: "1",
+    id: "next",
     type: "stack",
     data: { title: "Next.js", icon: <NextJSIcon size="100%" /> },
     position: { x: 0, y: 0 },
   },
 
   {
-    id: "2",
+    id: "tailwind",
     type: "stack",
     data: { title: "Tailwind", icon: <TailwindIcon size="100%" /> },
     position: { x: 0, y: 100 },
   },
 
   {
-    id: "3",
+    id: "tanstack",
     type: "stack",
     data: { title: "Tanstack Query", icon: <TanstackIcon size="100%" /> },
     position: { x: 150, y: 100 },
@@ -28,6 +28,6 @@ export const STACK_FLOW_NODES = [
 ];
 
 export const STACK_FLOW_EDGES = [
-  { id: "e1-2", source: "1", target: "2" },
-  { id: "e2-3", source: "1", target: "3" },
+  { id: "next-tailwind", source: "next", target: "tailwind" },
+  { id: "next-tanstack", source: "next", target: "tanstack" },
 ];
