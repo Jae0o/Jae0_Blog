7f6954dcf9c743d2798f879bbbd9985281ddd827
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-07 14:19:47 +0900

    ♻️ Refactor :  Hover Animate 추가

--- src/app/introduce/_containers/IntroFlow/components/FlowNodes/components/StackNode/StackNode.tsx
+++ src/app/introduce/_containers/IntroFlow/components/FlowNodes/components/StackNode/StackNode.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { Handle, Node, NodeProps, Position } from "@xyflow/react";
 
 import { NodeType } from "../../../../types";
 import { getNodeColor } from "../../../../utils";
+
+import { motion } from "motion/react";
 
 type StackNodeProps = Node<
   { title: string; icon: React.ReactNode; type: NodeType },
@@ -17,13 +21,14 @@ const StackNode = ({ data }: NodeProps<StackNodeProps>) => {
         position={Position.Top}
       />
 
-      <div
+      <motion.div
         className="w-[12rem] p-[0.4rem] flex items-center gap-[0.8rem] rounded-[0.8rem] border-[0.2rem] bg-white_500"
         style={{ borderColor: getNodeColor(data.type) }}
+        whileHover={{ scale: 1.1 }}
       >
         <span className="w-[2rem] h-[2rem]">{data.icon}</span>
         <p>{data.title}</p>
-      </div>
+      </motion.div>
 
       <Handle
         id={data.title}
--- src/app/introduce/_containers/IntroFlow/components/FlowNodes/components/TitleNode/TitleNode.tsx
+++ src/app/introduce/_containers/IntroFlow/components/FlowNodes/components/TitleNode/TitleNode.tsx
@@ -1,22 +1,27 @@
+"use client";
+
 import { Handle, Node } from "@xyflow/react";
 import { NodeProps, Position } from "@xyflow/system";
 
 import { NodeType } from "../../../../types";
 import { getNodeColor } from "../../../../utils";
 
+import { motion } from "motion/react";
+
 type TitleNodeProps = Node<{ title: string; type: NodeType }, "stack">;
 
 const TitleNode = ({ data }: NodeProps<TitleNodeProps>) => {
   return (
     <>
-      <div
+      <motion.div
         className="w-[12rem] p-[0.4rem] rounded-[0.8rem]"
         style={{ backgroundColor: getNodeColor(data.type) }}
+        whileHover={{ scale: 1.1 }}
       >
         <p className="w-full h-[2rem] flex items-center justify-center text-white_500 text-[1.2rem] font-bold">
           {data.title}
         </p>
-      </div>
+      </motion.div>
 
       <Handle
         id={data.title}
