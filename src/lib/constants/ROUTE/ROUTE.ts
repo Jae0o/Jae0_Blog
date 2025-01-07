0a46e1e42016348ae2d028feb38a839148b9ffba
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-07 15:58:54 +0900

    ✏️ Feat :  POST_DETAIL PATH 상수 추가

--- src/lib/constants/ROUTE/ROUTE.ts
+++ src/lib/constants/ROUTE/ROUTE.ts
@@ -2,4 +2,5 @@ export const ROUTE_PATH = {
   HOME: "/",
   NEW_EDIT: "/editor/newPost",
   POST_LIST: (category: string) => `/post/list/${category}`,
+  POST_DETAIL: (id: string) => `/post/detail/${id}`,
 } as const;
