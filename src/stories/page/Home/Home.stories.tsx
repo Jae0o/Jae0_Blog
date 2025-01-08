b4e85b4f1a1966ee0f33153921532e1f57ef5522
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-08 13:47:03 +0900

    ✏️ Feat :  HomePage Storybook 생성

---
+++ src/stories/page/Home/Home.stories.tsx
@@ -0,0 +1,41 @@
+import HomePage from "@/app/(Home)/page";
+import { TanstackProvider } from "@/app/_components";
+
+import type { Meta, StoryObj } from "@storybook/react";
+
+/**
+ * ## Home Page
+ *
+ * Home Route에서 사용되어지는 페이지입니다. <br>
+ *
+ * 내부에서 <br>
+ *
+ * - HomeTitle
+ * - HomeBannerList
+ * - HomePostList
+ *
+ * 각 컴포넌트를 사용합니다. <br>
+ *
+ * */
+const meta = {
+  title: "Page/Home",
+  component: HomePage,
+  parameters: {
+    layout: "centered",
+  },
+
+  decorators: [
+    Storybook => (
+      <TanstackProvider>
+        <Storybook />
+      </TanstackProvider>
+    ),
+  ],
+
+  tags: ["autodocs"],
+} satisfies Meta<typeof HomePage>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
