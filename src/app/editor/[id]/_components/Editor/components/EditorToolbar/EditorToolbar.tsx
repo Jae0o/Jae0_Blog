d060ef7dc7cadd8c01107b61357e5b73e2e0cd49
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-01-09 15:33:09 +0900

    ✏️ Feat :  Editor Bullet style

--- src/app/editor/[id]/_components/Editor/components/EditorToolbar/EditorToolbar.tsx
+++ src/app/editor/[id]/_components/Editor/components/EditorToolbar/EditorToolbar.tsx
@@ -8,71 +8,70 @@ interface EditorToolbarProps {
   editor: Editor;
 }
 
-const buttonStyle = "w-[2.4rem] h-[2.4rem] bg-red-100";
+interface ToolbarButtonProps {
+  children: React.ReactNode;
+  onClick: () => void;
+  isActive?: boolean;
+}
+
+const ToolbarButton = ({ children, onClick, isActive }: ToolbarButtonProps) => {
+  return (
+    <button
+      className={twMerge(
+        "w-[2.4rem] h-[2.4rem] bg-red-100",
+        isActive && "opacity-50",
+      )}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
 
 const EditorToolbar = ({ editor }: EditorToolbarProps) => {
   const handleToolbar = useCallback(() => editor.chain().focus(), [editor]);
 
   return (
-    <ul className="w-full h-[3.2rem] px-[1rem] flex items-center">
+    <ul className="w-full h-[3.2rem] px-[1rem] flex items-center gap-[2rem]">
       {/* ------ Heading Tool -------- */}
       <li className="flex items-center gap-[0.6rem]">
-        <button
-          className={twMerge(
-            buttonStyle,
-            editor.isActive("heading", { level: 1 }) && "opacity-50",
-          )}
+        <ToolbarButton
+          isActive={editor.isActive("heading", { level: 1 })}
           onClick={() => handleToolbar().toggleHeading({ level: 1 }).run()}
         >
           h1
-        </button>
+        </ToolbarButton>
 
-        <button
-          className={twMerge(
-            buttonStyle,
-            editor.isActive("heading", { level: 2 }) && "opacity-50",
-          )}
+        <ToolbarButton
+          isActive={editor.isActive("heading", { level: 2 })}
           onClick={() => handleToolbar().toggleHeading({ level: 2 }).run()}
         >
           h2
-        </button>
+        </ToolbarButton>
 
-        <button
-          className={twMerge(
-            buttonStyle,
-            editor.isActive("heading", { level: 3 }) && "opacity-50",
-          )}
+        <ToolbarButton
+          isActive={editor.isActive("heading", { level: 3 })}
           onClick={() => handleToolbar().toggleHeading({ level: 3 }).run()}
         >
           h3
-        </button>
+        </ToolbarButton>
       </li>
 
       {/* ------ Text Tool -------- */}
       <li className="flex items-center gap-[0.6rem]">
-        <button
-          className={twMerge(
-            buttonStyle,
-            editor.isActive("blockquote") && "opacity-50",
-          )}
+        <ToolbarButton
+          isActive={editor.isActive("blockquote")}
           onClick={() => handleToolbar().toggleBlockquote().run()}
         >
           Block
-        </button>
+        </ToolbarButton>
 
-        <button
-          className={buttonStyle}
-          onClick={() => handleToolbar().toggleHeading({ level: 2 }).run()}
+        <ToolbarButton
+          isActive={editor.isActive("bulletList")}
+          onClick={() => handleToolbar().toggleBulletList().run()}
         >
-          h2
-        </button>
-
-        <button
-          className={buttonStyle}
-          onClick={() => handleToolbar().toggleHeading({ level: 3 }).run()}
-        >
-          h3
-        </button>
+          bullet
+        </ToolbarButton>
       </li>
     </ul>
   );
