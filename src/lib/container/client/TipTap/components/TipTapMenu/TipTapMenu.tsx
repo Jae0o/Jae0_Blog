a5a4b26dd7602b7a3f7fe6808770343e11dd8e58
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-23 22:05:06 +0900

    ✏️ Feat :  useHeadingExtension

--- src/lib/container/client/TipTap/components/TipTapMenu/TipTapMenu.tsx
+++ src/lib/container/client/TipTap/components/TipTapMenu/TipTapMenu.tsx
@@ -1,14 +1,33 @@
 "use client";
 
+import { Button } from "@/lib/components/client";
+
 import { TipTapMenuProps } from "./TipTapMenu.type";
 import { useHeadingExtension } from "./hooks";
+
+import { twMerge } from "tailwind-merge";
 
 const TipTapMenu = ({ editor }: TipTapMenuProps) => {
   const headings = useHeadingExtension({ editor });
   console.log(headings);
 
   return (
-    <div className="w-full p-[1rem] border-2 border-red-500">TipTapMenu</div>
+    <ul className="w-full p-[1rem] flex items-center gap-[1.2rem] flex-wrap bg-gray_500 rounded-[1.2rem]">
+      <li className="flex items-center gap-[0.4rem]">
+        {headings.map(({ key, icon, run, isActive }) => (
+          <Button
+            key={key}
+            onClick={run}
+            className={twMerge(
+              "p-[0.6rem] bg-white shadow-lg",
+              isActive && "bg-yellow_500",
+            )}
+          >
+            {icon}
+          </Button>
+        ))}
+      </li>
+    </ul>
   );
 };
 
--- src/lib/container/client/TipTap/components/TipTapMenu/hooks/useHeadingExtension/useHeadingExtension.tsx
+++ src/lib/container/client/TipTap/components/TipTapMenu/hooks/useHeadingExtension/useHeadingExtension.tsx
@@ -1,11 +1,40 @@
 "use client";
 
-import { memo } from "react";
+import { useCallback } from "react";
+
+import {
+  TipTap_H1Icon,
+  TipTap_H2Icon,
+  TipTap_H3Icon,
+} from "@/lib/components/server";
 
 import { TipTapExtensionParams } from "../../../../TipTap.type";
 
-const useHeadingExtension = memo(({}: TipTapExtensionParams) => {
-  return <div>useHeadingExtension</div>;
-});
+const useHeadingExtension = ({ editor }: TipTapExtensionParams) => {
+  const handleToolbar = useCallback(() => editor.chain().focus(), [editor]);
+
+  const heading1 = {
+    isActive: editor.isActive("heading", { level: 1 }),
+    run: () => handleToolbar().toggleHeading({ level: 1 }).run(),
+    icon: <TipTap_H1Icon size="2.4rem" />,
+    key: "tiptap_heading_1",
+  };
+
+  const heading2 = {
+    isActive: editor.isActive("heading", { level: 2 }),
+    run: () => handleToolbar().toggleHeading({ level: 2 }).run(),
+    icon: <TipTap_H2Icon size="2.4rem" />,
+    key: "tiptap_heading_2",
+  };
+
+  const heading3 = {
+    isActive: editor.isActive("heading", { level: 3 }),
+    run: () => handleToolbar().toggleHeading({ level: 3 }).run(),
+    icon: <TipTap_H3Icon size="2.4rem" />,
+    key: "tiptap_heading_3",
+  };
+
+  return [heading1, heading2, heading3] as const;
+};
 
 export default useHeadingExtension;
