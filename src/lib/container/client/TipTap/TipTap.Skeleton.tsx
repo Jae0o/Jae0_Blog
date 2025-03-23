d01241e884338592becbf1377582d10714a3bc08
Lee Jae Yeong <127748428+Jae0o@users.noreply.github.com>
2025-03-23 18:27:14 +0900

    ✏️ Feat : TipTap Skeleton & Menubar 적용

---
+++ src/lib/container/client/TipTap/TipTap.Skeleton.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@/lib/components/server";
+
+const TipTapSkeleton = () => {
+  return <Skeleton className="w-full h-full" />;
+};
+
+export default TipTapSkeleton;
--- src/lib/container/client/TipTap/TipTap.tsx
+++ src/lib/container/client/TipTap/TipTap.tsx
@@ -3,6 +3,9 @@
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
+import TipTapSkeleton from "./TipTap.Skeleton";
+import { TipTapMenu } from "./components";
+
 const TipTap = () => {
   const editor = useEditor({
     content: "",
@@ -10,8 +13,12 @@ const TipTap = () => {
     extensions: [StarterKit],
   });
 
+  if (!editor) return <TipTapSkeleton />;
+
   return (
     <article className="w-full h-full flex flex-col">
+      <TipTapMenu editor={editor} />
+
       <EditorContent
         editor={editor}
         className="w-full grow-[1] border-2 border-red-600"
